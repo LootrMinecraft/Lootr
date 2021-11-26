d9a06c580e9dddb58411a2dd241283bd4c42b760
Jon McManus <due@wxwhatever.com>
2021-11-27 09:52:54 +1000

    Port #55 fix to 1.17.
    Player can be null.

--- src/main/java/noobanidus/mods/lootr/client/LootrChestBlockRenderer.java
+++ src/main/java/noobanidus/mods/lootr/client/LootrChestBlockRenderer.java
@@ -6,10 +6,12 @@ import net.minecraft.client.renderer.blockentity.ChestRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.state.properties.ChestType;
 import noobanidus.mods.lootr.Lootr;
 import noobanidus.mods.lootr.api.ILootTile;
 import noobanidus.mods.lootr.blocks.entities.LootrChestBlockEntity;
+import noobanidus.mods.lootr.util.Getter;
 
 import java.util.UUID;
 
@@ -26,7 +28,12 @@ public class LootrChestBlockRenderer<T extends LootrChestBlockEntity & ILootTile
   @Override
   protected Material getMaterial(T tile, ChestType type) {
     if (playerId == null) {
-      playerId = Minecraft.getInstance().player.getUUID();
+      Player player = Getter.getPlayer();
+      if (player != null) {
+        playerId = player.getUUID();
+      } else {
+        return MATERIAL;
+      }
     }
     if (tile.isOpened()) {
       return MATERIAL2;
