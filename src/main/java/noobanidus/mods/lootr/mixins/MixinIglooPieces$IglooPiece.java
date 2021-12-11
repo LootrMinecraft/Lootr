f43785c8d6af52efc77ce13aaa3494319aacf755
Jon McManus <due@wxwhatever.com>
2021-12-11 10:47:46 +1000

    Fix mixins, etc.

--- src/main/java/noobanidus/mods/lootr/mixins/MixinIglooPieces$IglooPiece.java
+++ src/main/java/noobanidus/mods/lootr/mixins/MixinIglooPieces$IglooPiece.java
@@ -1,17 +1,15 @@
 package noobanidus.mods.lootr.mixins;
 
-import net.minecraft.world.level.block.ChestBlock;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
-import net.minecraft.tags.FluidTags;
 import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.levelgen.structure.OceanRuinPieces;
+import net.minecraft.world.level.levelgen.structure.IglooPieces;
 import noobanidus.mods.lootr.config.ConfigManager;
-import noobanidus.mods.lootr.init.ModBlocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,23 +17,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
 
-@Mixin(OceanRuinPieces.OceanRuinPiece.class)
-public class MixinOceanRuinPiece {
+@Mixin(IglooPieces.IglooPiece.class)
+public class MixinIglooPieces$IglooPiece {
   @Inject(method = "handleDataMarker",
       at = @At(value = "HEAD"),
       cancellable = true)
   protected void handleDataMarker(String function, BlockPos pos, ServerLevelAccessor worldIn, Random rand, BoundingBox sbb, CallbackInfo info) {
+    if (ConfigManager.getLootBlacklist().contains(BuiltInLootTables.IGLOO_CHEST)) {
+      return;
+    }
     if ("chest".equals(function)) {
-      boolean large = ((OceanRuinPieces.OceanRuinPiece) (Object) this).isLarge;
-      if (ConfigManager.getLootBlacklist().contains(large ? BuiltInLootTables.UNDERWATER_RUIN_BIG : BuiltInLootTables.UNDERWATER_RUIN_SMALL)) {
-        return;
-      }
       ResourceKey<Level> key = worldIn.getLevel().dimension();
       if (ConfigManager.isDimensionBlocked(key)) {
         return;
       }
-      worldIn.setBlock(pos, ModBlocks.CHEST.defaultBlockState().setValue(ChestBlock.WATERLOGGED, worldIn.getFluidState(pos).is(FluidTags.WATER)), 2);
-      RandomizableContainerBlockEntity.setLootTable(worldIn, rand, pos, large ? BuiltInLootTables.UNDERWATER_RUIN_BIG : BuiltInLootTables.UNDERWATER_RUIN_SMALL);
+      worldIn.setBlock(pos, Blocks.AIR.defaultBlockState(), 3);
+      RandomizableContainerBlockEntity.setLootTable(worldIn, rand, pos.below(), BuiltInLootTables.IGLOO_CHEST);
       info.cancel();
     }
   }
