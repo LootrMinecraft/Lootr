86e07638ea7234a15bf99acccb8303adfca4ba8b
Jon McManus <due@wxwhatever.com>
2021-12-23 09:45:11 +1000

    Allow complete disabling of chest breaking.
    The only way to break containers or carts is to do so while sneaking in
    creative.
    
    Conflicts:
    	src/main/java/noobanidus/mods/lootr/events/HandleBreak.java

--- src/main/java/noobanidus/mods/lootr/events/HandleBreak.java
+++ src/main/java/noobanidus/mods/lootr/events/HandleBreak.java
@@ -1,13 +1,15 @@
 package noobanidus.mods.lootr.events;
 
 import com.google.common.collect.Sets;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.Util;
-import net.minecraft.network.chat.TextColor;
-import net.minecraft.network.chat.Style;
 import net.minecraft.ChatFormatting;
+import net.minecraft.Util;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.event.world.BlockEvent;
+import noobanidus.mods.lootr.config.ConfigManager;
 import noobanidus.mods.lootr.init.ModBlocks;
 
 import java.util.Set;
@@ -16,12 +18,26 @@ public class HandleBreak {
   public static Set<Block> specialLootChests = Sets.newHashSet(ModBlocks.CHEST, ModBlocks.BARREL, ModBlocks.TRAPPED_CHEST, ModBlocks.SHULKER, ModBlocks.INVENTORY);
 
   public static void onBlockBreak(BlockEvent.BreakEvent event) {
+    Player player = event.getPlayer();
+
     if (!event.getWorld().isClientSide()) {
       if (specialLootChests.contains(event.getState().getBlock())) {
-        if (!event.getPlayer().isShiftKeyDown()) {
-          event.setCanceled(true);
-          event.getPlayer().sendMessage(new TranslatableComponent("lootr.message.should_sneak").setStyle(Style.EMPTY.withColor(TextColor.fromLegacyFormat(ChatFormatting.AQUA))), Util.NIL_UUID);
-          event.getPlayer().sendMessage(new TranslatableComponent("lootr.message.should_sneak2", new TranslatableComponent("lootr.message.should_sneak3").setStyle(Style.EMPTY.withBold(true))).setStyle(Style.EMPTY.withColor(TextColor.fromLegacyFormat(ChatFormatting.AQUA))), Util.NIL_UUID);
+        if (ConfigManager.DISABLE_BREAK.get()) {
+          if (player.getAbilities().instabuild) {
+            if (!player.isShiftKeyDown()) {
+              event.setCanceled(true);
+              player.sendMessage(new TranslatableComponent("lootr.message.cannot_break_sneak").setStyle(Style.EMPTY.withColor(TextColor.fromLegacyFormat(ChatFormatting.AQUA))), Util.NIL_UUID);
+            }
+          } else {
+            event.setCanceled(true);
+            player.sendMessage(new TranslatableComponent("lootr.message.cannot_break").setStyle(Style.EMPTY.withColor(TextColor.fromLegacyFormat(ChatFormatting.AQUA))), Util.NIL_UUID);
+          }
+        } else {
+          if (!event.getPlayer().isShiftKeyDown()) {
+            event.setCanceled(true);
+            event.getPlayer().sendMessage(new TranslatableComponent("lootr.message.should_sneak").setStyle(Style.EMPTY.withColor(TextColor.fromLegacyFormat(ChatFormatting.AQUA))), Util.NIL_UUID);
+            event.getPlayer().sendMessage(new TranslatableComponent("lootr.message.should_sneak2", new TranslatableComponent("lootr.message.should_sneak3").setStyle(Style.EMPTY.withBold(true))).setStyle(Style.EMPTY.withColor(TextColor.fromLegacyFormat(ChatFormatting.AQUA))), Util.NIL_UUID);
+          }
         }
       }
     }
