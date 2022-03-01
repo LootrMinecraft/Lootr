5e10c406b55235fa1296b02ce45b8147d77946b5
Jon McManus <due@wxwhatever.com>
2022-03-01 12:25:39 +1000

    Automatically back-up data files during migration.

--- src/main/java/noobanidus/mods/lootr/event/HandleMigrate.java
+++ src/main/java/noobanidus/mods/lootr/event/HandleMigrate.java
@@ -7,19 +7,33 @@ import net.minecraftforge.fml.common.Mod;
 import noobanidus.mods.lootr.api.LootrAPI;
 import noobanidus.mods.lootr.data.DataStorage;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Mod.EventBusSubscriber(modid = LootrAPI.MODID)
 public class HandleMigrate {
+  private static void append(StringBuilder builder, int intVal) {
+    append(builder, intVal, true);
+  }
+
+  private static void append(StringBuilder builder, int intVal, boolean sep) {
+    if (intVal < 10) {
+      builder.append('0').append(intVal);
+    } else {
+      builder.append(intVal);
+    }
+    if (sep) {
+      builder.append('-');
+    }
+  }
+
   @SubscribeEvent
   public static void onServerStarting(ServerStartedEvent event) {
-    Map<Path, Path> migrations = new HashMap<>();
     List<Path> toMigrate;
 
     Path data = event.getServer().getWorldPath(new LevelResource("data"));
@@ -32,11 +46,84 @@ public class HandleMigrate {
       return;
     }
 
+    toMigrate.removeIf(path -> !path.getFileName().toString().startsWith("Lootr-"));
+
+    if (!toMigrate.isEmpty()) {
+      LootrAPI.LOG.info("Backing up {} files...", toMigrate.size());
+      Calendar time = Calendar.getInstance();
+      StringBuilder filenameBuilder = new StringBuilder();
+      filenameBuilder.append("LootrMigrationBackup-");
+      append(filenameBuilder, time.get(Calendar.YEAR));
+      append(filenameBuilder, time.get(Calendar.MONTH) + 1);
+      append(filenameBuilder, time.get(Calendar.DAY_OF_MONTH));
+      append(filenameBuilder, time.get(Calendar.HOUR_OF_DAY));
+      append(filenameBuilder, time.get(Calendar.MINUTE));
+      append(filenameBuilder, time.get(Calendar.SECOND), false);
+      String name = filenameBuilder.toString();
+      Path backupName = data.resolve(name + ".zip");
+      File backup = backupName.toFile();
+      int inc = 0;
+      while (backup.exists()) {
+        backupName = data.resolve(name + "-" + inc + ".zip");
+        backup = backupName.toFile();
+        if (inc++ >= 99) {
+          throw new IllegalStateException("Unable to create backup for Lootr data files. Reached '" + name + "-" + inc + ".zip' and all files exist!");
+        }
+      }
+
+      try {
+        backup.getParentFile().mkdirs();
+        if (!backup.createNewFile()) {
+          throw new IllegalStateException("Unable to create backup for Lootr data files! Couldn't create " + backup);
+        }
+      } catch (IOException e) {
+        throw new IllegalStateException("Unable to create backup for Lootr data files!", e);
+      }
+
+      ZipOutputStream backupZip;
+      try {
+        backupZip = new ZipOutputStream(new FileOutputStream(backup));
+      } catch (FileNotFoundException e) {
+        throw new IllegalStateException("Unable to create backup for Lootr data files!", e);
+      }
+
+      backupZip.setLevel(0);
+      boolean failure = false;
+      byte[] readBuffer = new byte[4096];
+      for (Path path : toMigrate) {
+        ZipEntry entry = new ZipEntry(path.getFileName().toString());
+        try {
+          backupZip.putNextEntry(entry);
+          FileInputStream inputStream = new FileInputStream(path.toFile());
+          int len;
+          while ((len = inputStream.read(readBuffer)) > 0) {
+            backupZip.write(readBuffer, 0, len);
+          }
+          backupZip.closeEntry();
+          inputStream.close();
+        } catch (IOException e) {
+          failure = true;
+          LootrAPI.LOG.error("Unable to fully back-up Lootr data, failure while reading: {}", path, e);
+        }
+      }
+
+      try {
+        backupZip.close();
+      } catch (IOException e) {
+        throw new IllegalStateException("Unable to close current back-up file: " + backup, e);
+      }
+
+      if (failure) {
+        throw new IllegalStateException("Unable to fully back-up Lootr data. Please check log file for details as to which file failed to back-up.");
+      } else {
+        LootrAPI.LOG.info("Completed backup! {} files were backed up to {}", toMigrate.size(), backup);
+      }
+    }
+
+    Map<Path, Path> migrations = new HashMap<>();
+
     for (Path path : toMigrate) {
       String fileName = path.getFileName().toString();
-      if (!fileName.startsWith("Lootr-")) {
-        continue;
-      }
 
       if (fileName.startsWith(DataStorage.ID_OLD) || fileName.startsWith(DataStorage.SCORED_OLD) || fileName.startsWith(DataStorage.DECAY_OLD) || fileName.startsWith(DataStorage.REFRESH_OLD)) {
         // Data files go into the subdirectory
