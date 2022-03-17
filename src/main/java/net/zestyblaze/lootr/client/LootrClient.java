cbc4c9e1b933ba98a891715baeb9fb9da89c7a47
Jon McManus <due@wxwhatever.com>
2022-03-18 08:07:21 +1000

    Item renderer for the chest block.

--- src/main/java/net/zestyblaze/lootr/client/LootrClient.java
+++ src/main/java/net/zestyblaze/lootr/client/LootrClient.java
@@ -4,10 +4,13 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.zestyblaze.lootr.api.LootrAPI;
 import net.zestyblaze.lootr.client.block.LootrChestBlockRenderer;
+import net.zestyblaze.lootr.client.item.LootrChestItemRenderer;
 import net.zestyblaze.lootr.config.LootrModConfig;
 import net.zestyblaze.lootr.registry.LootrBlockEntityInit;
+import net.zestyblaze.lootr.registry.LootrBlockInit;
 import net.zestyblaze.lootr.registry.LootrNetworkingInit;
 
 @Environment(EnvType.CLIENT)
@@ -17,6 +20,7 @@ public class LootrClient implements ClientModInitializer {
         LootrNetworkingInit.registerClientNetwork();
 
         BlockEntityRendererRegistry.register(LootrBlockEntityInit.SPECIAL_LOOT_CHEST, LootrChestBlockRenderer::new);
+        BuiltinItemRendererRegistry.INSTANCE.register(LootrBlockInit.CHEST, LootrChestItemRenderer.getInstance());
 
         if(LootrModConfig.get().debug.debugMode) {
             LootrAPI.LOG.info("Lootr: Registry - Client Fully Loaded!");
---
+++ src/main/java/net/zestyblaze/lootr/client/item/LootrChestItemRenderer.java
@@ -0,0 +1,57 @@
+package net.zestyblaze.lootr.client.item;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
+import net.zestyblaze.lootr.blocks.entities.LootrChestBlockEntity;
+import net.zestyblaze.lootr.registry.LootrBlockInit;
+
+public class LootrChestItemRenderer extends BlockEntityWithoutLevelRenderer implements BuiltinItemRendererRegistry.DynamicItemRenderer {
+  private static LootrChestItemRenderer INSTANCE = null;
+
+  private BlockEntityRenderDispatcher blockEntityRenderDispatcher;
+  private final LootrChestBlockEntity tile = new LootrChestBlockEntity(BlockPos.ZERO, LootrBlockInit.CHEST.defaultBlockState());
+
+  public LootrChestItemRenderer(BlockEntityRenderDispatcher pBlockEntityRenderDispatcher, EntityModelSet pEntityModelSet) {
+    super(pBlockEntityRenderDispatcher, pEntityModelSet);
+    this.blockEntityRenderDispatcher = pBlockEntityRenderDispatcher;
+  }
+
+  public LootrChestItemRenderer() {
+    this(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels());
+  }
+
+  @Override
+  public void renderByItem(ItemStack stack, ItemTransforms.TransformType p_239207_2_, PoseStack matrixStack, MultiBufferSource buffer, int combinedLight, int combinedOverlay) {
+    if (this.blockEntityRenderDispatcher == null) {
+      this.blockEntityRenderDispatcher = Minecraft.getInstance().getBlockEntityRenderDispatcher();
+    }
+    this.blockEntityRenderDispatcher.renderItem(tile, matrixStack, buffer, combinedLight, combinedOverlay);
+  }
+
+  public void render(ItemStack stack, ItemTransforms.TransformType mode, PoseStack matrices, MultiBufferSource vertexConsumers, int light, int overlay) {
+    renderByItem(stack, mode, matrices, vertexConsumers, light, overlay);
+  }
+
+/*  public void renderByMinecart (LootrChestMinecartEntity entity, PoseStack matrixStack, MultiBufferSource buffer, int combinedLight) {
+    boolean open = tile.isOpened();
+    tile.setOpened(entity.isOpened());
+    this.blockEntityRenderDispatcher.renderItem(tile, matrixStack, buffer, combinedLight, OverlayTexture.NO_OVERLAY);
+    tile.setOpened(open);
+  }*/
+
+  public static LootrChestItemRenderer getInstance() {
+    if (INSTANCE == null) {
+      INSTANCE = new LootrChestItemRenderer();
+    }
+
+    return INSTANCE;
+  }
+}
