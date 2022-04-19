55bb870048228fe8550778a95fb34fcd5fd1f155
Jon McManus <due@wxwhatever.com>
2022-04-19 12:27:42 +1000

    Obtain entityId earlier in the process.
    Trying to read from the bytebuf during the client execution process
    results in reference count issues.

--- src/main/java/net/zestyblaze/lootr/registry/LootrNetworkingInit.java
+++ src/main/java/net/zestyblaze/lootr/registry/LootrNetworkingInit.java
@@ -9,25 +9,29 @@ import net.zestyblaze.lootr.network.NetworkConstants;
 
 public class LootrNetworkingInit {
     public static void registerClientNetwork() {
-        ClientPlayNetworking.registerGlobalReceiver(NetworkConstants.CLOSE_CART_CHANNEL, (client, handler, buf, responseSender) -> client.execute(() -> {
+        ClientPlayNetworking.registerGlobalReceiver(NetworkConstants.CLOSE_CART_CHANNEL, (client, handler, buf, responseSender) -> {
             int entityId = buf.readVarInt();
-            if (client.player != null && client.player.level != null) {
-                Entity potential = client.player.level.getEntity(entityId);
-                if (potential instanceof LootrChestMinecartEntity cart) {
-                    cart.setClosed();
+            client.execute(() -> {
+                if (client.player != null && client.player.level != null) {
+                    Entity potential = client.player.level.getEntity(entityId);
+                    if (potential instanceof LootrChestMinecartEntity cart) {
+                        cart.setClosed();
+                    }
                 }
-            }
-        }));
+            });
+        });
 
-        ClientPlayNetworking.registerGlobalReceiver(NetworkConstants.OPEN_CART_CHANNEL, (client, handler, buf, responseSender) -> client.execute(() -> {
+        ClientPlayNetworking.registerGlobalReceiver(NetworkConstants.OPEN_CART_CHANNEL, (client, handler, buf, responseSender) -> {
             int entityId = buf.readVarInt();
-            if (client.player != null && client.player.level != null) {
-                Entity potential = client.player.level.getEntity(entityId);
-                if (potential instanceof LootrChestMinecartEntity cart) {
-                    cart.setOpened();
+            client.execute(() -> {
+                if (client.player != null && client.player.level != null) {
+                    Entity potential = client.player.level.getEntity(entityId);
+                    if (potential instanceof LootrChestMinecartEntity cart) {
+                        cart.setOpened();
+                    }
                 }
-            }
-        }));
+            });
+        });
 
         if(LootrModConfig.get().debug.debugMode) {
             LootrAPI.LOG.info("Lootr: Client Registry - Networking Registered");
