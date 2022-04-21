9d029c0e6767bbc7b0de2a3fe2021951fe89bc76
Jon McManus <due@wxwhatever.com>
2022-04-21 12:23:36 +1000

    Get ready for beta release.

--- src/main/java/net/zestyblaze/lootr/Lootr.java
+++ src/main/java/net/zestyblaze/lootr/Lootr.java
@@ -2,7 +2,11 @@ package net.zestyblaze.lootr;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.zestyblaze.lootr.api.LootrAPI;
@@ -10,24 +14,24 @@ import net.zestyblaze.lootr.config.LootrModConfig;
 import net.zestyblaze.lootr.registry.*;
 
 public class Lootr implements ModInitializer {
-	public static CreativeModeTab TAB = FabricItemGroupBuilder.build(new ResourceLocation(LootrAPI.MODID, LootrAPI.MODID), () -> new ItemStack(LootrItemInit.CHEST));
+  public static CreativeModeTab TAB = FabricItemGroupBuilder.build(new ResourceLocation(LootrAPI.MODID, LootrAPI.MODID), () -> new ItemStack(LootrItemInit.CHEST));
 
-	@Override
-	public void onInitialize() {
-		LootrAPI.LOG.info("Lootr is installed, loading now! Thanks for installing! <3");
-		LootrConfigInit.registerConfig();
-		LootrItemInit.registerItems();
-		LootrBlockInit.registerBlocks();
-		LootrBlockEntityInit.registerBlockEntities();
-		LootrEntityInit.registerEntities();
-		LootrLootInit.registerLoot();
-		LootrEventsInit.registerEvents();
-		LootrStatsInit.registerStats();
-		LootrAdvancementsInit.registerAdvancements();
-		LootrCommandInit.registerCommands();
+  @Override
+  public void onInitialize() {
+    LootrAPI.LOG.info("Lootr is installed, loading now! Thanks for installing! <3");
+    LootrConfigInit.registerConfig();
+    LootrItemInit.registerItems();
+    LootrBlockInit.registerBlocks();
+    LootrBlockEntityInit.registerBlockEntities();
+    LootrEntityInit.registerEntities();
+    LootrLootInit.registerLoot();
+    LootrEventsInit.registerEvents();
+    LootrStatsInit.registerStats();
+    LootrAdvancementsInit.registerAdvancements();
+    LootrCommandInit.registerCommands();
 
-		if(LootrModConfig.get().debug.debugMode) {
-			LootrAPI.LOG.info("Lootr: Registry - Mod Fully Loaded!");
-		}
-	}
+    if (LootrModConfig.get().debug.debugMode) {
+      LootrAPI.LOG.info("Lootr: Registry - Mod Fully Loaded!");
+    }
+  }
 }
