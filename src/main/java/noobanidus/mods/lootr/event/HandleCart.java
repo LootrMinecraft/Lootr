40c5dd461537793fdbe921c346ea65fe57a0796b
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-08-04 08:00:38 -0400

    Tweak entity replacement logic

--- src/main/java/noobanidus/mods/lootr/event/HandleCart.java
+++ src/main/java/noobanidus/mods/lootr/event/HandleCart.java
@@ -4,19 +4,37 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityMinecartChest;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import noobanidus.mods.lootr.Lootr;
 import noobanidus.mods.lootr.config.ConfigManager;
-import noobanidus.mods.lootr.entity.EntityTicker;
 import noobanidus.mods.lootr.entity.LootrChestMinecartEntity;
 import noobanidus.mods.lootr.networking.OpenCart;
 import noobanidus.mods.lootr.networking.PacketHandler;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.WeakHashMap;
+
 @Mod.EventBusSubscriber(modid = Lootr.MODID)
 public class HandleCart {
+  private static Set<Chunk> loadedChunks = Collections.newSetFromMap(new WeakHashMap<Chunk, Boolean>());
+
+  @SubscribeEvent
+  public static void chunkLoad(ChunkEvent.Load event) {
+    loadedChunks.add(event.getChunk());
+  }
+
+  @SubscribeEvent
+  public static void chunkUnload(ChunkEvent.Unload event) {
+    loadedChunks.remove(event.getChunk());
+  }
+
   @SubscribeEvent
   public static void onEntityJoin(EntityJoinWorldEvent event) {
     if (event.getEntity() instanceof EntityMinecartChest) {
@@ -24,9 +42,16 @@ public class HandleCart {
       if (!chest.world.isRemote && chest.lootTable != null && ConfigManager.CONVERT_MINESHAFTS && !ConfigManager.isBlacklisted(chest.lootTable)) {
         LootrChestMinecartEntity lootr = new LootrChestMinecartEntity(chest.world, chest.posX, chest.posY, chest.posZ);
         lootr.setLootTable(chest.lootTable, chest.lootTableSeed);
-        lootr.forceSpawn = true;
-        chest.world.spawnEntity(lootr);
+        int chunkX = MathHelper.floor(chest.posX) >> 4;
+        int chunkZ = MathHelper.floor(chest.posZ) >> 4;
+        Chunk chunk = chest.world.getChunk(chunkX, chunkZ);
+        if(loadedChunks.contains(chunk)) {
+          chest.world.spawnEntity(lootr);
+        } else {
+          chunk.addEntity(lootr);
+        }
         event.setCanceled(true);
+        chest.setDead();
         //EntityTicker.addEntity(lootr);
       }
     }
