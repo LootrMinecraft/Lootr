c6448aa7f1871e699d4baa49903c44d663451544
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-11-06 12:16:40 -0500

    Add missing event handler for decay and refresh
    Fixes #144

--- src/main/java/noobanidus/mods/lootr/setup/CommonSetup.java
+++ src/main/java/noobanidus/mods/lootr/setup/CommonSetup.java
@@ -3,7 +3,10 @@ package noobanidus.mods.lootr.setup;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import noobanidus.mods.lootr.Lootr;
+import noobanidus.mods.lootr.data.DataStorage;
 import noobanidus.mods.lootr.networking.PacketHandler;
 
 @Mod.EventBusSubscriber(modid = Lootr.MODID)
@@ -15,6 +18,14 @@ public abstract class CommonSetup {
     public abstract EntityPlayer getPlayer();
   public void init() {
       PacketHandler.registerMessages();
+  }
+
+  @SubscribeEvent
+  public static void serverTick(TickEvent.ServerTickEvent e) {
+      if(e.phase == TickEvent.Phase.END) {
+          DataStorage.doDecay();
+          DataStorage.doRefresh();
+      }
   }
 
   public abstract void changeCartStatus(int entityId, boolean status);
