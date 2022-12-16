9415b357beb33fb5a06244cc6e0d0fca541af3c6
Jon McManus <noobanidus@users.noreply.github.com>
2022-12-16 11:59:15 +1000

    Fix pose check vs shift-key check.

--- src/main/java/noobanidus/mods/lootr/block/LootrInventoryBlock.java
+++ src/main/java/noobanidus/mods/lootr/block/LootrInventoryBlock.java
@@ -54,7 +54,7 @@ public class LootrInventoryBlock extends ChestBlock {
 
   @Override
   public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult trace) {
-    if (player.isCrouching()) {
+    if (player.isShiftKeyDown()) {
       ChestUtil.handleLootSneak(this, world, pos, player);
     } else if (!ChestBlock.isChestBlockedAt(world, pos)) {
       ChestUtil.handleLootInventory(this, world, pos, player);
