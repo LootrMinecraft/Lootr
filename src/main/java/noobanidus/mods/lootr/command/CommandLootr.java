080814efedd5d71ebf6b0eeb1a2282f85066c7d1
Jon McManus <noobanidus@users.noreply.github.com>
2023-03-31 17:02:00 +1000

    Port barrel changes from Fabric.

--- src/main/java/noobanidus/mods/lootr/command/CommandLootr.java
+++ src/main/java/noobanidus/mods/lootr/command/CommandLootr.java
@@ -7,7 +7,6 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import net.minecraft.ChatFormatting;
-import net.minecraft.Util;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.SharedSuggestionProvider;
@@ -16,15 +15,19 @@ import net.minecraft.commands.arguments.coordinates.Vec3Argument;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
-import net.minecraft.core.Vec3i;
-import net.minecraft.network.chat.*;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentUtils;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BarrelBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ChestBlock;
+import net.minecraft.world.level.block.entity.BarrelBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
@@ -83,7 +86,7 @@ public class CommandLootr {
 
   public static void createBlock(CommandSourceStack c, @Nullable Block block, @Nullable ResourceLocation table) {
     Level world = c.getLevel();
-    Vec3  incomingPos = c.getPosition();
+    Vec3 incomingPos = c.getPosition();
     BlockPos pos = new BlockPos((int) incomingPos.x, (int) incomingPos.y, (int) incomingPos.z);
     if (table == null) {
       table = getTables().get(world.getRandom().nextInt(getTables().size()));
@@ -185,26 +188,36 @@ public class CommandLootr {
       return 1;
     })));
     builder.then(Commands.literal("custom").executes(c -> {
-      Vec3 incomingPos = c.getSource().getPosition();
-      BlockPos pos = new BlockPos((int) incomingPos.x, (int) incomingPos.y, (int) incomingPos.z);
+      BlockPos pos = BlockPos.containing(c.getSource().getPosition());
       Level world = c.getSource().getLevel();
       BlockState state = world.getBlockState(pos);
-      if (!state.is(Blocks.CHEST)) {
+      if (!state.is(Blocks.CHEST) && !state.is(Blocks.BARREL)) {
         pos = pos.below();
         state = world.getBlockState(pos);
       }
-      if (!state.is(Blocks.CHEST)) {
-        c.getSource().sendSuccess(Component.literal("Please stand on the chest you wish to convert."), false);
+      if (!state.is(Blocks.CHEST) && !state.is(Blocks.BARREL)) {
+        c.getSource().sendSuccess(Component.literal("Please stand on the chest or barrel you wish to convert."), false);
       } else {
-        NonNullList<ItemStack> reference = ((ChestBlockEntity) Objects.requireNonNull(world.getBlockEntity(pos))).items;
+        NonNullList<ItemStack> reference;
+        BlockState newState;
+        if (state.is(Blocks.CHEST)) {
+          reference = ((ChestBlockEntity) Objects.requireNonNull(world.getBlockEntity(pos))).items;
+          newState = ModBlocks.INVENTORY.get().defaultBlockState().setValue(ChestBlock.FACING, state.getValue(ChestBlock.FACING)).setValue(ChestBlock.WATERLOGGED, state.getValue(ChestBlock.WATERLOGGED));
+        } else {
+          Direction facing = state.getValue(BarrelBlock.FACING);
+          if (facing == Direction.UP || facing == Direction.DOWN) {
+            facing = Direction.NORTH;
+          }
+          reference = ((BarrelBlockEntity) Objects.requireNonNull(world.getBlockEntity(pos))).items;
+          newState = ModBlocks.INVENTORY.get().defaultBlockState().setValue(ChestBlock.FACING, facing);
+        }
         NonNullList<ItemStack> custom = ChestUtil.copyItemList(reference);
         world.removeBlockEntity(pos);
-        world.setBlockAndUpdate(pos, ModBlocks.INVENTORY.get().defaultBlockState().setValue(ChestBlock.FACING, state.getValue(ChestBlock.FACING)).setValue(ChestBlock.WATERLOGGED, state.getValue(ChestBlock.WATERLOGGED)));
+        world.setBlockAndUpdate(pos, newState);
         BlockEntity te = world.getBlockEntity(pos);
-        if (!(te instanceof LootrInventoryBlockEntity)) {
+        if (!(te instanceof LootrInventoryBlockEntity inventory)) {
           c.getSource().sendSuccess(Component.literal("Unable to convert chest, BlockState is not a Lootr Inventory block."), false);
         } else {
-          LootrInventoryBlockEntity inventory = (LootrInventoryBlockEntity) te;
           inventory.setCustomInventory(custom);
           inventory.setChanged();
         }
@@ -212,8 +225,7 @@ public class CommandLootr {
       return 1;
     }));
     builder.then(Commands.literal("id").executes(c -> {
-      Vec3 incomingPos = c.getSource().getPosition();
-      BlockPos pos = new BlockPos((int) incomingPos.x, (int) incomingPos.y, (int) incomingPos.z);
+      BlockPos pos = BlockPos.containing(c.getSource().getPosition());
       Level world = c.getSource().getLevel();
       BlockEntity te = world.getBlockEntity(pos);
       if (!(te instanceof ILootBlockEntity)) {
@@ -228,8 +240,7 @@ public class CommandLootr {
       return 1;
     }));
     builder.then(Commands.literal("refresh").executes(c -> {
-      Vec3 incomingPos = c.getSource().getPosition();
-      BlockPos pos = new BlockPos((int) incomingPos.x, (int) incomingPos.y, (int) incomingPos.z);
+      BlockPos pos = BlockPos.containing(c.getSource().getPosition());
       Level level = c.getSource().getLevel();
       BlockEntity be = level.getBlockEntity(pos);
       if (!(be instanceof ILootBlockEntity)) {
@@ -237,16 +248,15 @@ public class CommandLootr {
         be = level.getBlockEntity(pos);
       }
       if (be instanceof ILootBlockEntity) {
-        DataStorage.setRefreshing(((ILootBlockEntity)be).getTileId(), ConfigManager.REFRESH_VALUE.get());
-        c.getSource().sendSuccess(Component.literal("Container with ID " + ((ILootBlockEntity)be).getTileId() + " has been set to refresh with a delay of " + ConfigManager.REFRESH_VALUE.get()), false);
+        DataStorage.setRefreshing(((ILootBlockEntity) be).getTileId(), ConfigManager.REFRESH_VALUE.get());
+        c.getSource().sendSuccess(Component.literal("Container with ID " + ((ILootBlockEntity) be).getTileId() + " has been set to refresh with a delay of " + ConfigManager.REFRESH_VALUE.get()), false);
       } else {
         c.getSource().sendSuccess(Component.literal("Please stand on a valid Lootr container."), false);
       }
       return 1;
     }));
     builder.then(Commands.literal("decay").executes(c -> {
-      Vec3 incomingPos = c.getSource().getPosition();
-      BlockPos pos = new BlockPos((int) incomingPos.x, (int) incomingPos.y, (int) incomingPos.z);
+      BlockPos pos = BlockPos.containing(c.getSource().getPosition());
       Level level = c.getSource().getLevel();
       BlockEntity be = level.getBlockEntity(pos);
       if (!(be instanceof ILootBlockEntity)) {
@@ -254,8 +264,8 @@ public class CommandLootr {
         be = level.getBlockEntity(pos);
       }
       if (be instanceof ILootBlockEntity) {
-        DataStorage.setDecaying(((ILootBlockEntity)be).getTileId(), ConfigManager.DECAY_VALUE.get());
-        c.getSource().sendSuccess(Component.literal("Container with ID " + ((ILootBlockEntity)be).getTileId() + " has been set to decay with a delay of " + ConfigManager.DECAY_VALUE.get()), false);
+        DataStorage.setDecaying(((ILootBlockEntity) be).getTileId(), ConfigManager.DECAY_VALUE.get());
+        c.getSource().sendSuccess(Component.literal("Container with ID " + ((ILootBlockEntity) be).getTileId() + " has been set to decay with a delay of " + ConfigManager.DECAY_VALUE.get()), false);
       } else {
         c.getSource().sendSuccess(Component.literal("Please stand on a valid Lootr container."), false);
       }
