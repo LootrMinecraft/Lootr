28d07385e2ab09289d23be53d949d7c34ba3b804
Jon McManus <due@wxwhatever.com>
2023-04-02 13:04:11 +1000

    Make world border checks configurable.
    Fix illogic.
    
    Option now defaults to true.
    
    Temporarily make default, fix sidedness bug.
    
    Extraneous chunk check.
    
    Clarify chunk checks with comments.

--- src/main/java/noobanidus/mods/lootr/mixins/MixinLevelChunk.java
+++ src/main/java/noobanidus/mods/lootr/mixins/MixinLevelChunk.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.level.chunk.LevelChunk;
 import noobanidus.mods.lootr.api.blockentity.ILootBlockEntity;
 import noobanidus.mods.lootr.block.entities.TileTicker;
+import noobanidus.mods.lootr.config.ConfigManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,9 +17,17 @@ public class MixinLevelChunk {
   private void lootrUpdateBlockEntityTicker(BlockEntity entity, CallbackInfo cir) {
     if (entity instanceof RandomizableContainerBlockEntity && !(entity instanceof ILootBlockEntity)) {
       LevelChunk level = (LevelChunk) (Object) this;
-      if (!level.getLevel().isClientSide() && level.getLevel().getWorldBorder().isWithinBounds(entity.getBlockPos())) {
-        TileTicker.addEntry(level.getLevel(), entity.getBlockPos());
+      if (level.getLevel().isClientSide()) {
+        return;
       }
+      // By default block entities outside of the world border are
+      // not converted. When the world border changes, you will
+      // need to restart the server.
+      if (ConfigManager.CHECK_WORLD_BORDER.get() && !level.getLevel().getWorldBorder().isWithinBounds(entity.getBlockPos())) {
+        return;
+      }
+
+      TileTicker.addEntry(level.getLevel(), entity.getBlockPos());
     }
   }
 }
