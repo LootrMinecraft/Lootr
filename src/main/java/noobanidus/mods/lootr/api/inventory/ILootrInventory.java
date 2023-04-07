d2241bfe04e07d5e1662733fef651c9ec46639e6
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-04-07 15:50:44 -0400

    Fix #173 for 1.12
    Not sure if it's actually an issue here but just in case

--- src/main/java/noobanidus/mods/lootr/api/inventory/ILootrInventory.java
+++ src/main/java/noobanidus/mods/lootr/api/inventory/ILootrInventory.java
@@ -21,6 +21,6 @@ public interface ILootrInventory extends IInventory, IInteractionObject {
   @Nullable
   BlockPos getPos();
 
-  NonNullList<ItemStack> getContents();
+  NonNullList<ItemStack> getInventoryContents();
 
 }
