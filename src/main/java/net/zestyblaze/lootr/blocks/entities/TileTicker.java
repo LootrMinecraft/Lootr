fbcc25ad133acbec05027787e622ec4d7f19203e
Jon McManus <noobanidus@users.noreply.github.com>
2023-05-30 10:10:34 +1000

    Bump version, optimize stuff.

--- src/main/java/net/zestyblaze/lootr/blocks/entities/TileTicker.java
+++ src/main/java/net/zestyblaze/lootr/blocks/entities/TileTicker.java
@@ -1,5 +1,6 @@
 package net.zestyblaze.lootr.blocks.entities;
 
+import it.unimi.dsi.fastutil.objects.ObjectLinkedOpenHashSet;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -24,8 +25,8 @@ public class TileTicker {
   private final static Object listLock = new Object();
   private final static Object worldLock = new Object();
   private static boolean tickingList = false;
-  private final static Set<Entry> tileEntries = new LinkedHashSet<>();
-  private final static Set<Entry> pendingEntries = new LinkedHashSet<>();
+  private final static Set<Entry> tileEntries = new ObjectLinkedOpenHashSet<>();
+  private final static Set<Entry> pendingEntries = new ObjectLinkedOpenHashSet<>();
 
   public static void addEntry(ResourceKey<Level> dimension, BlockPos position) {
     // TODO: Dimension Blacklisting
@@ -42,26 +43,42 @@ public class TileTicker {
     }
   }
 
+  private static final int MAX_AGE = 90 * 20;
+
   public static void serverTick() {
       Set<Entry> toRemove = new HashSet<>();
       Set<Entry> copy;
       synchronized (listLock) {
         tickingList = true;
-        copy = new HashSet<>(tileEntries);
+        copy = new ObjectLinkedOpenHashSet<>(tileEntries);
         tickingList = false;
       }
       synchronized (worldLock) {
         MinecraftServer server = ServerAccessImpl.getServer();
         for (Entry entry : copy) {
+          if (entry.age(server) > MAX_AGE) {
+            toRemove.add(entry);
+            continue;
+          }
           ServerLevel level = server.getLevel(entry.getDimension());
           if (level == null) {
             toRemove.add(entry);
             continue;
           }
+          boolean skip = false;
           synchronized (HandleChunk.LOADED_CHUNKS) {
-            if (HandleChunk.LOADED_CHUNKS.get(entry.dimension) == null || !HandleChunk.LOADED_CHUNKS.get(entry.dimension).containsAll(entry.getChunkPositions())) {
-              continue;
+            Set<ChunkPos> loadedChunks = HandleChunk.LOADED_CHUNKS.get(entry.dimension);
+            if (loadedChunks != null) {
+              for (ChunkPos chunkPos : entry.getChunkPositions()) {
+                if (!loadedChunks.contains(chunkPos)) {
+                  skip = true;
+                  break;
+                }
+              }
             }
+          }
+          if (skip) {
+            continue;
           }
           BlockEntity blockEntity = level.getBlockEntity(entry.getPosition());
           if (!(blockEntity instanceof RandomizableContainerBlockEntity be) || blockEntity instanceof ILootBlockEntity) {
@@ -119,6 +136,7 @@ public class TileTicker {
     private final ResourceKey<Level> dimension;
     private final BlockPos position;
     private final Set<ChunkPos> chunks = new HashSet<>();
+    private final long addedAt;
 
     public Entry(ResourceKey<Level> dimension, BlockPos position) {
       this.dimension = dimension;
@@ -135,6 +153,8 @@ public class TileTicker {
           chunks.add(new ChunkPos(oX + x, oZ + z));
         }
       }
+
+      this.addedAt = ServerAccessImpl.getServer().getTickCount();
     }
 
     public ResourceKey<Level> getDimension() {
@@ -149,6 +169,9 @@ public class TileTicker {
       return chunks;
     }
 
+    public long age(MinecraftServer server) {
+      return server.getTickCount() - addedAt;
+    }
     @Override
     public boolean equals(Object o) {
       if (this == o) return true;
--- src/main/java/net/zestyblaze/lootr/chunk/HandleChunk.java
+++ src/main/java/net/zestyblaze/lootr/chunk/HandleChunk.java
@@ -1,5 +1,7 @@
 package net.zestyblaze.lootr.chunk;
 
+import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.objects.ObjectLinkedOpenHashSet;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
@@ -10,13 +12,13 @@ import net.minecraft.world.level.chunk.LevelChunk;
 import java.util.*;
 
 public class HandleChunk {
-  public static final Map<ResourceKey<Level>, Set<ChunkPos>> LOADED_CHUNKS = Collections.synchronizedMap(new HashMap<>());
+  public static final Map<ResourceKey<Level>, Set<ChunkPos>> LOADED_CHUNKS = Collections.synchronizedMap(new Object2ObjectLinkedOpenHashMap<>());
 
   public static void onChunkLoad(ServerLevel level, LevelChunk chunk) {
     if (!level.isClientSide()) {
       if (chunk.getStatus().isOrAfter(ChunkStatus.FULL)) {
         synchronized (LOADED_CHUNKS) {
-          Set<ChunkPos> chunkSet = LOADED_CHUNKS.computeIfAbsent(chunk.getLevel().dimension(), k -> Collections.synchronizedSet(new HashSet<>()));
+          Set<ChunkPos> chunkSet = LOADED_CHUNKS.computeIfAbsent(chunk.getLevel().dimension(), k -> Collections.synchronizedSet(new ObjectLinkedOpenHashSet<>()));
           chunkSet.add(chunk.getPos());
         }
       }
