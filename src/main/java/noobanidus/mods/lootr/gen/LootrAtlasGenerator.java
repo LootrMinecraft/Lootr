6453075e5c0d48295987e8a9b1c4af4c37ba174b
Jon McManus <noobanidus@users.noreply.github.com>
2023-06-15 20:15:00 +1000

    Fix atlas issues.
    These textures shouldn't be in the main block atlas, but I'll fix that
    in an upcoming release.

--- src/main/java/noobanidus/mods/lootr/gen/LootrAtlasGenerator.java
+++ src/main/java/noobanidus/mods/lootr/gen/LootrAtlasGenerator.java
@@ -2,6 +2,7 @@ package noobanidus.mods.lootr.gen;
 
 import net.minecraft.client.renderer.texture.atlas.sources.SingleFile;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.common.data.SpriteSourceProvider;
 import noobanidus.mods.lootr.api.LootrAPI;
@@ -17,9 +18,12 @@ public class LootrAtlasGenerator extends SpriteSourceProvider {
 
   @Override
   protected void addSources() {
-    this.atlas(CHESTS_ATLAS).addSource(new SingleFile(LootrChestBlockRenderer.MATERIAL.texture(), Optional.empty()));
-    this.atlas(CHESTS_ATLAS).addSource(new SingleFile(LootrChestBlockRenderer.MATERIAL2.texture(), Optional.empty()));
-    this.atlas(SHULKER_BOXES_ATLAS).addSource(new SingleFile(LootrShulkerBlockRenderer.MATERIAL.texture(), Optional.empty()));
-    this.atlas(SHULKER_BOXES_ATLAS).addSource(new SingleFile(LootrShulkerBlockRenderer.MATERIAL2.texture(), Optional.empty()));
+    // TODO: Move these to the proper atlas locatiosn
+    this.atlas(BLOCKS_ATLAS).addSource(new SingleFile(LootrChestBlockRenderer.MATERIAL.texture(), Optional.empty()));
+    this.atlas(BLOCKS_ATLAS).addSource(new SingleFile(LootrChestBlockRenderer.MATERIAL2.texture(), Optional.empty()));
+    this.atlas(BLOCKS_ATLAS).addSource(new SingleFile(LootrShulkerBlockRenderer.MATERIAL.texture(), Optional.empty()));
+    this.atlas(BLOCKS_ATLAS).addSource(new SingleFile(LootrShulkerBlockRenderer.MATERIAL2.texture(), Optional.empty()));
+    this.atlas(BLOCKS_ATLAS).addSource(new SingleFile(new ResourceLocation(LootrAPI.MODID, "chest_opened"), Optional.empty()));
+    this.atlas(BLOCKS_ATLAS).addSource(new SingleFile(new ResourceLocation(LootrAPI.MODID, "entity/player/wide/steve"), Optional.empty()));
   }
 }
