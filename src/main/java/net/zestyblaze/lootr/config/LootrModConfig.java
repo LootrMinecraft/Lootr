1f6747c867f47b093bd4e78f2c5aabaa2a7854c4
Jon McManus <due@wxwhatever.com>
2023-06-18 13:30:51 +1000

    Remove debug mode comments.

--- src/main/java/net/zestyblaze/lootr/config/LootrModConfig.java
+++ src/main/java/net/zestyblaze/lootr/config/LootrModConfig.java
@@ -13,7 +13,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ChestBlock;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
