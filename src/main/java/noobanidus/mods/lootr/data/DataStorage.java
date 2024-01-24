385426ebe432417af55f34a454824baaf1d84841
Jon <due@wxwhatever.com>
2024-01-24 15:32:26 +1000

    Missing import.

--- src/main/java/noobanidus/mods/lootr/data/DataStorage.java
+++ src/main/java/noobanidus/mods/lootr/data/DataStorage.java
@@ -6,6 +6,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
