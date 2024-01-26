5a8ed4424158d922387bc705e69ace8ee0edb620
Jon <due@wxwhatever.com>
2024-01-26 12:16:07 +1000

    Revert "Revert "Trapped chest shenanigans.""
    This reverts commit cbcb2d4043d17c7da26647d2334e17ffe94b2429.

--- src/main/java/noobanidus/mods/lootr/client/block/LootrChestBlockRenderer.java
+++ src/main/java/noobanidus/mods/lootr/client/block/LootrChestBlockRenderer.java
@@ -7,11 +7,13 @@ import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.entity.TrappedChestBlockEntity;
 import net.minecraft.world.level.block.state.properties.ChestType;
 import noobanidus.mods.lootr.api.LootrAPI;
 import noobanidus.mods.lootr.api.blockentity.ILootBlockEntity;
 import noobanidus.mods.lootr.block.entities.LootrChestBlockEntity;
 import noobanidus.mods.lootr.config.ConfigManager;
+import noobanidus.mods.lootr.init.ModBlockEntities;
 import noobanidus.mods.lootr.util.Getter;
 
 import java.util.UUID;
@@ -21,6 +23,8 @@ public class LootrChestBlockRenderer<T extends LootrChestBlockEntity & ILootBloc
   private UUID playerId = null;
   public static final Material MATERIAL = new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(LootrAPI.MODID, "chest"));
   public static final Material MATERIAL2 = new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(LootrAPI.MODID, "chest_opened"));
+  public static final Material MATERIAL3 = new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(LootrAPI.MODID, "trapped_chest"));
+  public static final Material MATERIAL4 = new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(LootrAPI.MODID, "trapped_chest_opened"));
 
   public LootrChestBlockRenderer(BlockEntityRendererProvider.Context p_173607_) {
     super(p_173607_);
@@ -28,24 +32,25 @@ public class LootrChestBlockRenderer<T extends LootrChestBlockEntity & ILootBloc
 
   @Override
   protected Material getMaterial(T tile, ChestType type) {
+    boolean trapped = tile.getType() == ModBlockEntities.LOOTR_TRAPPED_CHEST.get();
     if (ConfigManager.isVanillaTextures()) {
-      return Sheets.chooseMaterial(tile, type, false);
+      return trapped ? Sheets.CHEST_TRAP_LOCATION : Sheets.CHEST_LOCATION;
     }
     if (playerId == null) {
       Player player = Getter.getPlayer();
       if (player != null) {
         playerId = player.getUUID();
       } else {
-        return MATERIAL;
+        return trapped ? MATERIAL3 : MATERIAL;
       }
     }
     if (tile.isOpened()) {
-      return MATERIAL2;
+      return trapped ? MATERIAL4 : MATERIAL2;
     }
     if (tile.getOpeners().contains(playerId)) {
-      return MATERIAL2;
+      return trapped ? MATERIAL4 : MATERIAL2;
     } else {
-      return MATERIAL;
+      return trapped ? MATERIAL3 : MATERIAL;
     }
   }
 }
