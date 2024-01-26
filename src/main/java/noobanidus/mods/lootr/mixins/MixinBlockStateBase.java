f871cb5c698f7c8b3fdcd78887f013ded465bdf4
Jon <due@wxwhatever.com>
2024-01-26 12:14:04 +1000

    Fix horifically broken mixin.

--- src/main/java/noobanidus/mods/lootr/mixins/MixinBlockStateBase.java
+++ src/main/java/noobanidus/mods/lootr/mixins/MixinBlockStateBase.java
@@ -3,6 +3,8 @@ package noobanidus.mods.lootr.mixins;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import noobanidus.mods.lootr.LootrTags;
 import noobanidus.mods.lootr.config.ConfigManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +15,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinBlockStateBase {
   @Inject(method="getDestroySpeed", at=@At("HEAD"), cancellable=true)
   private void LootrBreakSpeed (BlockGetter p_60801_, BlockPos p_60802_, CallbackInfoReturnable<Float> cir) {
-    if (ConfigManager.DISABLE_BREAK.get()) {
+    BlockBehaviour.BlockStateBase state = (BlockBehaviour.BlockStateBase) (Object) this;
+    if (state.is(LootrTags.Blocks.CONTAINERS) && ConfigManager.DISABLE_BREAK.get()) {
       cir.setReturnValue(-1f);
       cir.cancel();
     }
