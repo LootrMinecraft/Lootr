a5743e443cb878c4b8c61603ba1abf27fd7b8401
Jon <due@wxwhatever.com>
2024-02-09 15:06:27 +1000

    Properly make configs refresh after changes.
    This allows us to not require a restart for changing dimension modid
    blacklists, etc.

--- src/main/java/net/zestyblaze/lootr/registry/LootrConfigInit.java
+++ src/main/java/net/zestyblaze/lootr/registry/LootrConfigInit.java
@@ -1,12 +1,23 @@
 package net.zestyblaze.lootr.registry;
 
 import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.ConfigHolder;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import net.minecraft.world.InteractionResult;
 import net.zestyblaze.lootr.api.LootrAPI;
 import net.zestyblaze.lootr.config.LootrModConfig;
 
 public class LootrConfigInit {
     public static void registerConfig() {
         AutoConfig.register(LootrModConfig.class, GsonConfigSerializer::new);
+        ConfigHolder<LootrModConfig> config = AutoConfig.getConfigHolder(LootrModConfig.class);
+        config.registerLoadListener((manager, configData) -> {
+            configData.reset();
+            return InteractionResult.PASS;
+        });
+        config.registerSaveListener((manager, configData) -> {
+            configData.reset();
+            return InteractionResult.PASS;
+        });
     }
 }
