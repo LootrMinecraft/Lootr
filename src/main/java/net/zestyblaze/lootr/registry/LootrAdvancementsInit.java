2fe4a081e873e919c908360c344779e0e8fb3915
Jon <due@wxwhatever.com>
2024-02-21 14:03:34 +1000

    Properly register advancements.

--- src/main/java/net/zestyblaze/lootr/registry/LootrAdvancementsInit.java
+++ src/main/java/net/zestyblaze/lootr/registry/LootrAdvancementsInit.java
@@ -1,18 +1,28 @@
 package net.zestyblaze.lootr.registry;
 
 import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.zestyblaze.lootr.advancement.AdvancementTrigger;
 import net.zestyblaze.lootr.advancement.ContainerTrigger;
 import net.zestyblaze.lootr.advancement.LootedStatTrigger;
+import net.zestyblaze.lootr.api.LootrAPI;
 
 public class LootrAdvancementsInit {
-  public static AdvancementTrigger ADVANCEMENT_PREDICATE = CriteriaTriggers.register("advancement_predicate", new AdvancementTrigger());
-  public static ContainerTrigger CHEST_PREDICATE = CriteriaTriggers.register("chest_predicate", new ContainerTrigger());
-  public static ContainerTrigger BARREL_PREDICATE = CriteriaTriggers.register("barrel_predicate", new ContainerTrigger());
-  public static ContainerTrigger CART_PREDICATE = CriteriaTriggers.register("cart_predicate", new ContainerTrigger());
-  public static ContainerTrigger SHULKER_PREDICATE = CriteriaTriggers.register("shulker_predicate", new ContainerTrigger());
-  public static LootedStatTrigger SCORE_PREDICATE = CriteriaTriggers.register("score_predicate", new LootedStatTrigger());
+  public static AdvancementTrigger ADVANCEMENT_PREDICATE;
+  public static ContainerTrigger CHEST_PREDICATE;
+  public static ContainerTrigger BARREL_PREDICATE;
+  public static ContainerTrigger CART_PREDICATE;
+  public static ContainerTrigger SHULKER_PREDICATE;
+  public static LootedStatTrigger SCORE_PREDICATE;
 
   public static void registerAdvancements() {
+    ADVANCEMENT_PREDICATE = Registry.register(BuiltInRegistries.TRIGGER_TYPES, new ResourceLocation(LootrAPI.MODID, "advancement"), new AdvancementTrigger());
+    CHEST_PREDICATE = Registry.register(BuiltInRegistries.TRIGGER_TYPES, new ResourceLocation(LootrAPI.MODID, "chest_opened"), new ContainerTrigger());
+    BARREL_PREDICATE = Registry.register(BuiltInRegistries.TRIGGER_TYPES, new ResourceLocation(LootrAPI.MODID, "barrel_opened"), new ContainerTrigger());
+    CART_PREDICATE = Registry.register(BuiltInRegistries.TRIGGER_TYPES, new ResourceLocation(LootrAPI.MODID, "cart_opened"), new ContainerTrigger());
+    SHULKER_PREDICATE = Registry.register(BuiltInRegistries.TRIGGER_TYPES, new ResourceLocation(LootrAPI.MODID, "shulker_opened"), new ContainerTrigger());
+    SCORE_PREDICATE = Registry.register(BuiltInRegistries.TRIGGER_TYPES, new ResourceLocation(LootrAPI.MODID, "score"), new LootedStatTrigger());
   }
 }
