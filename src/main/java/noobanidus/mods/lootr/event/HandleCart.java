a00c3767a09fe5b92ab4dd284154ef07828b2d57
Jon <due@wxwhatever.com>
2024-03-08 13:12:53 +1000

    Test for off-thread cart creation.

--- src/main/java/noobanidus/mods/lootr/event/HandleCart.java
+++ src/main/java/noobanidus/mods/lootr/event/HandleCart.java
@@ -1,5 +1,10 @@
 package noobanidus.mods.lootr.event;
 
+<<<<<<< HEAD
+=======
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+>>>>>>> 28fbef8 (Test for off-thread cart creation.)
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.MinecartChest;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -19,10 +24,17 @@ public class HandleCart {
     }
     if (event.getEntity().getType() == EntityType.CHEST_MINECART && event.getEntity() instanceof MinecartChest chest) {
       if (!chest.level().isClientSide && chest.lootTable != null && ConfigManager.CONVERT_MINESHAFTS.get() && !ConfigManager.getLootBlacklist().contains(chest.lootTable)) {
-        LootrChestMinecartEntity lootr = new LootrChestMinecartEntity(chest.level(), chest.getX(), chest.getY(), chest.getZ());
-        lootr.setLootTable(chest.lootTable, chest.lootTableSeed);
-        event.setCanceled(true);
-        event.getLevel().addFreshEntity(lootr);
+        if (chest.level() instanceof ServerLevel level) {
+          LootrChestMinecartEntity lootr = new LootrChestMinecartEntity(chest.level, chest.getX(), chest.getY(), chest.getZ());
+          lootr.setLootTable(chest.lootTable, chest.lootTableSeed);
+          event.setCanceled(true);
+          if (!level.getServer().isSameThread()) {
+            LootrAPI.LOG.error("Minecart with Loot table created off main thread. Falling back on EntityTicker.");
+            EntityTicker.addEntity(lootr);
+           } else {
+            level.addFreshEntity(lootr);
+            }
+        }
       }
     }
   }
