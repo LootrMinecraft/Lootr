8d3bb3d8730356e87432b8ec3ba2c76d1a417676
Jon <due@wxwhatever.com>
2024-03-11 11:18:14 +1000

    Completely re-implement Poi stuff for 1.19.
    This is kind of gross but there's not really much that can be done
    without causing crashes with registry disparity or handling registry
    callbacks that are cleared before the game properly starts.

--- src/main/java/noobanidus/mods/lootr/setup/CommonSetup.java
+++ src/main/java/noobanidus/mods/lootr/setup/CommonSetup.java
@@ -1,6 +1,5 @@
 package noobanidus.mods.lootr.setup;
 
-import com.google.common.collect.ImmutableSet;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.Registry;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -10,29 +9,14 @@ import noobanidus.mods.lootr.advancement.AdvancementPredicate;
 import noobanidus.mods.lootr.advancement.ContainerPredicate;
 import noobanidus.mods.lootr.advancement.GenericTrigger;
 import noobanidus.mods.lootr.advancement.LootedStatPredicate;
-import net.minecraft.resources.ResourceLocation;
-
-import net.minecraft.world.entity.ai.village.poi.PoiType;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.GameData;
-import noobanidus.mods.lootr.advancement.*;
 import noobanidus.mods.lootr.api.LootrAPI;
 import noobanidus.mods.lootr.impl.LootrAPIImpl;
 import noobanidus.mods.lootr.init.ModAdvancements;
-import noobanidus.mods.lootr.init.ModBlocks;
 import noobanidus.mods.lootr.init.ModLoot;
 import noobanidus.mods.lootr.init.ModStats;
 import noobanidus.mods.lootr.network.PacketHandler;
 
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-
-import java.util.HashSet;
-import java.util.Set;
-
-@Mod.EventBusSubscriber(modid= LootrAPI.MODID, bus= Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = LootrAPI.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CommonSetup {
   @SubscribeEvent
   public static void init(FMLCommonSetupEvent event) {
@@ -49,14 +33,6 @@ public class CommonSetup {
       ModStats.load();
       ModAdvancements.SCORE_PREDICATE = CriteriaTriggers.register(new GenericTrigger<>(ModAdvancements.SCORE_LOCATION, new LootedStatPredicate()));
       PacketHandler.registerMessages();
-
-      PoiType fisherman = ForgeRegistries.POI_TYPES.getValue(new ResourceLocation("minecraft", "fisherman"));
-      Set<BlockState> states = new HashSet<>(fisherman.matchingStates);
-      states.addAll(ModBlocks.BARREL.get().getStateDefinition().getPossibleStates());
-      fisherman.matchingStates = ImmutableSet.copyOf(states);
-      for (BlockState state : ModBlocks.BARREL.get().getStateDefinition().getPossibleStates()) {
-        GameData.getBlockStatePointOfInterestTypeMap().put(state, fisherman);
-      }
     });
   }
 }
