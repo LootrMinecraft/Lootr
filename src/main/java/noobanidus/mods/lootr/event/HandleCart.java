605f35f95caaab4e2448de3f75dec01d22acb3ef
Jon <due@wxwhatever.com>
2024-03-11 12:23:05 +1000

    Fix a missing level reference.

--- src/main/java/noobanidus/mods/lootr/event/HandleCart.java
+++ src/main/java/noobanidus/mods/lootr/event/HandleCart.java
@@ -1,10 +1,6 @@
 package noobanidus.mods.lootr.event;
 
-<<<<<<< HEAD
-=======
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Entity;
->>>>>>> 28fbef8 (Test for off-thread cart creation.)
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.MinecartChest;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -25,16 +21,16 @@ public class HandleCart {
     if (event.getEntity().getType() == EntityType.CHEST_MINECART && event.getEntity() instanceof MinecartChest chest) {
       if (!chest.level().isClientSide && chest.lootTable != null && ConfigManager.CONVERT_MINESHAFTS.get() && !ConfigManager.getLootBlacklist().contains(chest.lootTable)) {
         if (chest.level() instanceof ServerLevel level) {
-          LootrChestMinecartEntity lootr = new LootrChestMinecartEntity(chest.level, chest.getX(), chest.getY(), chest.getZ());
+          LootrChestMinecartEntity lootr = new LootrChestMinecartEntity(chest.level(), chest.getX(), chest.getY(), chest.getZ());
           lootr.setLootTable(chest.lootTable, chest.lootTableSeed);
           event.setCanceled(true);
           if (!level.getServer().isSameThread()) {
             // TODO: If this is actually triggering, we need to ticket the chunk. The only instance I can think of this happening is if a mod is manually creating this event, as the Forge defaults only fire it in the main thread.
             LootrAPI.LOG.error("Minecart with Loot table created off main thread. Falling back on EntityTicker.");
             EntityTicker.addEntity(lootr);
-           } else {
+          } else {
             level.addFreshEntity(lootr);
-            }
+          }
         }
       }
     }
