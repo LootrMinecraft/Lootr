f4aadf42fb0839cdf958f796207c56496734b0b8
Jon <due@wxwhatever.com>
2024-03-11 12:23:05 +1000

    Fix a missing level reference.

--- src/main/java/noobanidus/mods/lootr/event/HandleCart.java
+++ src/main/java/noobanidus/mods/lootr/event/HandleCart.java
@@ -1,16 +1,11 @@
 package noobanidus.mods.lootr.event;
 
-<<<<<<< HEAD
-=======
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Entity;
->>>>>>> 28fbef8 (Test for off-thread cart creation.)
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.MinecartChest;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.server.permission.PermissionAPI;
 import noobanidus.mods.lootr.api.LootrAPI;
 import noobanidus.mods.lootr.config.ConfigManager;
 import noobanidus.mods.lootr.entity.EntityTicker;
@@ -26,7 +21,7 @@ public class HandleCart {
     if (event.getEntity().getType() == EntityType.CHEST_MINECART && event.getEntity() instanceof MinecartChest chest) {
       if (!chest.level().isClientSide && chest.lootTable != null && ConfigManager.CONVERT_MINESHAFTS.get() && !ConfigManager.getLootBlacklist().contains(chest.lootTable)) {
         if (chest.level() instanceof ServerLevel level) {
-          LootrChestMinecartEntity lootr = new LootrChestMinecartEntity(chest.level, chest.getX(), chest.getY(), chest.getZ());
+          LootrChestMinecartEntity lootr = new LootrChestMinecartEntity(chest.level(), chest.getX(), chest.getY(), chest.getZ());
           lootr.setLootTable(chest.lootTable, chest.lootTableSeed);
           event.setCanceled(true);
           if (!level.getServer().isSameThread()) {
