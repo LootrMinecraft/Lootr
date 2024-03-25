3d32d8cd796156aa97a139c01b9a7a51e9e782e2
Jon <due@wxwhatever.com>
2024-03-25 15:05:14 +1000

    Stop using DistExecutor.

--- src/main/java/noobanidus/mods/lootr/util/Getter.java
+++ src/main/java/noobanidus/mods/lootr/util/Getter.java
@@ -1,7 +1,9 @@
 package noobanidus.mods.lootr.util;
 
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import noobanidus.mods.lootr.client.impl.ClientGetter;
 import noobanidus.mods.lootr.impl.ServerGetter;
 
@@ -10,6 +12,10 @@ import javax.annotation.Nullable;
 public class Getter {
   @Nullable
   public static Player getPlayer() {
-    return DistExecutor.safeRunForDist(() -> ClientGetter::getPlayer, () -> ServerGetter::getPlayer);
+    if (FMLEnvironment.dist == Dist.CLIENT) {
+      return ClientGetter.getPlayer();
+    } else {
+      return ServerGetter.getPlayer();
+    }
   }
 }
