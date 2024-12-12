9a51a697cdcb01e0af0a2e9a3c3dc7b9ffa549be
Jon <due@wxwhatever.com>
2024-12-12 12:14:19 +1000

    Start porting for 1.21.4.

---
+++ common/src/main/java/noobanidus/mods/lootr/common/client/special/LootrChestSpecialRenderer.java
@@ -0,0 +1,4 @@
+package noobanidus.mods.lootr.common.client.special;
+
+public class LootrChestSpecialRenderer implements {
+}
--- neoforge/src/main/java/noobanidus/mods/lootr/neoforge/gen/LootrDataGenerators.java
+++ neoforge/src/main/java/noobanidus/mods/lootr/neoforge/gen/LootrDataGenerators.java
@@ -1,13 +1,8 @@
 package noobanidus.mods.lootr.neoforge.gen;
 
-import net.minecraft.DetectedVersion;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.metadata.PackMetadataGenerator;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.data.AdvancementProvider;
@@ -15,30 +10,36 @@ import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import noobanidus.mods.lootr.common.api.LootrAPI;
 
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 @EventBusSubscriber(modid = LootrAPI.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class LootrDataGenerators {
   @SubscribeEvent
-  public static void gatherData(GatherDataEvent event) {
+  public static void gatherData(GatherDataEvent.Client event) {
+    DataGenerator generator = event.getGenerator();
+    PackOutput output = event.getGenerator().getPackOutput();
+    CompletableFuture<HolderLookup.Provider> provider = event.getLookupProvider();
+    ExistingFileHelper helper = event.getExistingFileHelper();
+
+    generator.addProvider(true, new LootrAtlasGenerator(output, provider, helper));
+    generator.addProvider(true, new LootrLangProvider(output));
+  }
+
+  @SubscribeEvent
+  public static void gatherServerData(GatherDataEvent.Server event) {
     DataGenerator generator = event.getGenerator();
     PackOutput output = event.getGenerator().getPackOutput();
     CompletableFuture<HolderLookup.Provider> provider = event.getLookupProvider();
     ExistingFileHelper helper = event.getExistingFileHelper();
 
     LootrBlockTagProvider blocks;
-    generator.addProvider(event.includeServer(), blocks = new LootrBlockTagProvider(output, provider, helper));
-    generator.addProvider(event.includeServer(), new LootrItemTagsProvider(output, provider, blocks.contentsGetter(), helper));
-    generator.addProvider(event.includeClient(), new LootrAtlasGenerator(output, provider, helper));
+    generator.addProvider(true, blocks = new LootrBlockTagProvider(output, provider, helper));
+    generator.addProvider(true, new LootrItemTagsProvider(output, provider, blocks.contentsGetter(), helper));
     generator.addProvider(true, LootrLootTableProvider.create(output, provider));
-    generator.addProvider(event.includeServer(), new LootrEntityTagsProvider(output, provider, helper));
-    generator.addProvider(event.includeServer(), new LootrBlockEntityTagsProvider(output, provider, helper));
-    generator.addProvider(event.includeServer(), new LootrStructureTagsProvider(output, provider, helper));
-    generator.addProvider(event.includeClient(), new LootrLangProvider(output));
-    generator.addProvider(event.includeServer(), new AdvancementProvider(output, provider, helper, List.of(new LootrAdvancementGenerator())));
+    generator.addProvider(true, new LootrEntityTagsProvider(output, provider, helper));
+    generator.addProvider(true, new LootrBlockEntityTagsProvider(output, provider, helper));
+    generator.addProvider(true, new LootrStructureTagsProvider(output, provider, helper));
+    generator.addProvider(true, new AdvancementProvider(output, provider, helper, List.of(new LootrAdvancementGenerator())));
   }
 }
