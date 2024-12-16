da670f1565ad7456a38894603958212eafe57b0b
Jon <due@wxwhatever.com>
2024-12-16 13:01:41 +1000

    Render minecarts properly.

--- common/src/main/java/noobanidus/mods/lootr/common/client/entity/LootrChestCartRenderer.java
+++ common/src/main/java/noobanidus/mods/lootr/common/client/entity/LootrChestCartRenderer.java
@@ -1,19 +1,26 @@
 package noobanidus.mods.lootr.common.client.entity;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.model.ChestModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.AbstractMinecartRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.resources.model.Material;
 import net.minecraft.world.level.block.state.BlockState;
+import noobanidus.mods.lootr.common.client.block.LootrChestBlockRenderer;
 import noobanidus.mods.lootr.common.entity.LootrChestMinecartEntity;
 
 public class LootrChestCartRenderer<T extends LootrChestMinecartEntity> extends AbstractMinecartRenderer<T, LootrCartRenderState> {
-
+  private final ChestModel chestModel;
 
   public LootrChestCartRenderer(EntityRendererProvider.Context context, ModelLayerLocation modelLayerLocation) {
     super(context, modelLayerLocation);
+    this.chestModel = new ChestModel(context.bakeLayer(ModelLayers.CHEST));
   }
 
   @Override
@@ -23,9 +30,10 @@ public class LootrChestCartRenderer<T extends LootrChestMinecartEntity> extends 
 
   @Override
   protected void renderMinecartContents(LootrCartRenderState minecartRenderState, BlockState blockState, PoseStack poseStack, MultiBufferSource multiBufferSource, int i) {
-    super.renderMinecartContents(minecartRenderState, blockState, poseStack, multiBufferSource, i);
-    // TODO:
-    // LootrChestItemRenderer.getInstance().renderByMinecart(minecartRenderState, poseStack, multiBufferSource, i);
+    Material material = LootrChestBlockRenderer.getMaterial(false, minecartRenderState.open);
+    VertexConsumer consumer = material.buffer(multiBufferSource, RenderType::entitySolid);
+    this.chestModel.setupAnim(0f);
+    this.chestModel.renderToBuffer(poseStack, consumer, i, OverlayTexture.NO_OVERLAY);
   }
 
   @Override
