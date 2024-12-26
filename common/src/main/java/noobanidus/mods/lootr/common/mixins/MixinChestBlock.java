04ed1f75108de563e1a82ce50b69801002320a7b
Jon <due@wxwhatever.com>
2024-12-26 11:49:58 +1000

    Limit non-blocking blocks to only Lootr containers.

--- common/src/main/java/noobanidus/mods/lootr/common/mixins/MixinChestBlock.java
+++ common/src/main/java/noobanidus/mods/lootr/common/mixins/MixinChestBlock.java
@@ -4,6 +4,7 @@ import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.ChestBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import noobanidus.mods.lootr.common.api.LootrTags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,7 +15,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinChestBlock {
   @Inject(method = "isBlockedChestByBlock", at = @At("HEAD"), cancellable = true)
   private static void LootrIsChestBlocked(BlockGetter blockGetter, BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
-    if (blockGetter.getBlockState(blockPos.above()).is(LootrTags.Blocks.NON_BLOCKING)) {
+    BlockState thisState = blockGetter.getBlockState(blockPos);
+    BlockState aboveState = blockGetter.getBlockState(blockPos.above());
+    if (thisState.is(LootrTags.Blocks.CONTAINERS) && aboveState.is(LootrTags.Blocks.NON_BLOCKING)) {
       cir.setReturnValue(false);
       cir.cancel();
     }
