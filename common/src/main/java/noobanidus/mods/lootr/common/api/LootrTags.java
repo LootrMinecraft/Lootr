9d4c8c46e24195ad4202ef52de447c05bb18282b
Jon <due@wxwhatever.com>
2025-01-14 11:12:28 +1000

    Revert "Conversion tags for archaeology."
    This reverts commit 5e21889ed2e2ccd9e7e439a00df3284c904a3196.

--- common/src/main/java/noobanidus/mods/lootr/common/api/LootrTags.java
+++ common/src/main/java/noobanidus/mods/lootr/common/api/LootrTags.java
@@ -13,8 +13,6 @@ public class LootrTags {
     public static final TagKey<Block> CONVERT_TRAPPED_CHESTS = tag("convert/trapped_chests");
     public static final TagKey<Block> CONVERT_SHULKERS = tag("convert/shulkers");
     public static final TagKey<Block> CONVERT_BARRELS = tag("convert/barrels");
-    public static final TagKey<Block> CONVERT_SAND = tag("convert/sands");
-    public static final TagKey<Block> CONVERT_GRAVEL = tag("convert/gravels");
     public static final TagKey<Block> CONVERT_BLOCK = tag("convert/blocks");
     public static final TagKey<Block> CONVERT_BLACKLIST = tag("convert/blacklist");
 
@@ -23,7 +21,6 @@ public class LootrTags {
     public static final TagKey<Block> SHULKERS = tag("shulkers");
     public static final TagKey<Block> BARRELS = tag("barrels");
     public static final TagKey<Block> CONTAINERS = tag("containers");
-    public static final TagKey<Block> BLOCKS = tag("blocks");
 
     public static final TagKey<Block> CUSTOM_ELIGIBLE = tag("convert/custom_eligible");
 
@@ -42,7 +39,6 @@ public class LootrTags {
     public static TagKey<Item> SHULKERS = tag("shulkers");
     public static TagKey<Item> BARRELS = tag("barrels");
     public static TagKey<Item> CONTAINERS = tag("containers");
-    public static TagKey<Item> BLOCKS = tag("blocks");
 
     static TagKey<Item> tag(String name) {
       return TagKey.create(Registries.ITEM, LootrAPI.rl(name));
@@ -61,7 +57,6 @@ public class LootrTags {
   }
 
   public static class BlockEntity extends LootrTags {
-    @Deprecated
     public static TagKey<BlockEntityType<?>> LOOTR_OBJECT = tag("object");
     public static TagKey<BlockEntityType<?>> TRAPPED = tag("trapped");
     public static TagKey<BlockEntityType<?>> CUSTOM_INELIGIBlE = tag("custom_ineligible");
