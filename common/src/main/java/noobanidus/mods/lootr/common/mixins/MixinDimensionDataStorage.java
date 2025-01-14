f72c293826d6a3e223b571a7ebc883b084a7e687
Jon <due@wxwhatever.com>
2025-01-14 11:24:49 +1000

    Revert "Might revert this: "timeout" to unload Lootr data."
    This reverts commit 646b0be387cdec4642e3992c1bdf049dfb1803ea.

--- common/src/main/java/noobanidus/mods/lootr/common/mixins/MixinDimensionDataStorage.java
+++ common/src/main/java/noobanidus/mods/lootr/common/mixins/MixinDimensionDataStorage.java
@@ -2,41 +2,13 @@ package noobanidus.mods.lootr.common.mixins;
 
 import net.minecraft.world.level.saveddata.SavedData;
 import net.minecraft.world.level.storage.DimensionDataStorage;
-import noobanidus.mods.lootr.common.data.LootrSavedData;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 @Mixin(DimensionDataStorage.class)
-public class MixinDimensionDataStorage {
-  @Shadow
-  @Final
-  private Map<String, SavedData> cache;
-
-  @Inject(method="save",at=@At("TAIL"))
-  private void save (CallbackInfo ci) {
-    Set<String> toUnload = new HashSet<>();
-    for (Map.Entry<String, SavedData> entry : cache.entrySet()) {
-      if (entry.getValue() instanceof LootrSavedData lootrSavedData) {
-        if (lootrSavedData.isDirty()) {
-          continue;
-        }
-
-        if (lootrSavedData.shouldUnload()) {
-          toUnload.add(entry.getKey());
-        }
-      }
-    }
-    for (String toRemove : toUnload) {
-      cache.remove(toRemove);
-    }
-
-  }
+public interface MixinDimensionDataStorage {
+  @Accessor
+  Map<String, SavedData> getCache();
 }
