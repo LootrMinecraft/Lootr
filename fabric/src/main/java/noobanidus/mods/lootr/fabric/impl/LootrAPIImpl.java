5d8dd8a1e3c77664e45634bc51ea4d810fa359e3
Jon <due@wxwhatever.com>
2025-10-28 18:57:32 +1000

    Test improving chunk loading performance.

--- fabric/src/main/java/noobanidus/mods/lootr/fabric/impl/LootrAPIImpl.java
+++ fabric/src/main/java/noobanidus/mods/lootr/fabric/impl/LootrAPIImpl.java
@@ -18,12 +18,10 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootTable;
 import noobanidus.mods.lootr.common.api.client.ClientTextureType;
 import noobanidus.mods.lootr.common.api.data.ILootrInfoProvider;
-import noobanidus.mods.lootr.common.config.Replacements;
 import noobanidus.mods.lootr.common.impl.DefaultLootrAPIImpl;
 import noobanidus.mods.lootr.fabric.config.ConfigManager;
 import noobanidus.mods.lootr.fabric.event.HandleChunk;
 import noobanidus.mods.lootr.fabric.event.LootrEventsInit;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
@@ -348,21 +346,22 @@ public class LootrAPIImpl extends DefaultLootrAPIImpl {
 
   @Override
   public Component getInvalidTableComponent(ResourceKey<LootTable> lootTable) {
-    return Component.translatable("lootr.message.invalid_table", lootTable.location().getNamespace(), lootTable.toString()).setStyle(!isMessageStylesEnabled() ? Style.EMPTY : Style.EMPTY.withColor(TextColor.fromLegacyFormat(ChatFormatting.DARK_RED)).withBold(true));
+    return Component.translatable("lootr.message.invalid_table", lootTable.location()
+            .getNamespace(), lootTable.toString())
+        .setStyle(!isMessageStylesEnabled() ? Style.EMPTY : Style.EMPTY.withColor(TextColor.fromLegacyFormat(ChatFormatting.DARK_RED))
+            .withBold(true));
   }
 
   @Override
   public boolean anyUnloadedChunks(ResourceKey<Level> dimension, Set<ChunkPos> chunks) {
-    synchronized (HandleChunk.LOADED_CHUNKS) {
-      Set<ChunkPos> syncedChunks = HandleChunk.LOADED_CHUNKS.get(dimension);
-      if (syncedChunks == null || syncedChunks.isEmpty()) {
-        return true;
-      }
+    Set<ChunkPos> syncedChunks = HandleChunk.LOADED_CHUNKS.get(dimension);
+    if (syncedChunks == null || syncedChunks.isEmpty()) {
+      return true;
+    }
 
-      for (ChunkPos myPos : chunks) {
-        if (!syncedChunks.contains(myPos)) {
-          return true;
-        }
+    for (ChunkPos myPos : chunks) {
+      if (!syncedChunks.contains(myPos)) {
+        return true;
       }
     }
 
