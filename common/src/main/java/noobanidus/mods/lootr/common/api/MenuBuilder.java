bcce5736f4c03fc88403e46e0658d109e2a821cf
Jon <due@wxwhatever.com>
2025-12-04 15:23:26 +1000

    Document MenuBuilder.

--- common/src/main/java/noobanidus/mods/lootr/common/api/MenuBuilder.java
+++ common/src/main/java/noobanidus/mods/lootr/common/api/MenuBuilder.java
@@ -5,6 +5,11 @@ import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 
+/**
+ * This (optional) functional interface is used by `LootrInventory` to create
+ * menus. If not provided, generic chest menus will be used, guessed from the
+ * size of the container itself, as found in `LootrInventory::createMenu`.
+ */
 @FunctionalInterface
 public interface MenuBuilder {
   AbstractContainerMenu build(int id, Inventory inventory, Container container, int rows);
