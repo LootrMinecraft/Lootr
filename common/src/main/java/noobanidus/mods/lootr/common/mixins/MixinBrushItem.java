c4af98ef2bb06c88996dfd38de0232412ba044e2
Jon <due@wxwhatever.com>
2025-12-08 18:09:44 +1000

    Fix MixinBrushItem.
    Extrapolate the arguments instead of trying to use @Local on them. That
    seems like an inefficient method that constantly breaks.

--- common/src/main/java/noobanidus/mods/lootr/common/mixins/MixinBrushItem.java
+++ common/src/main/java/noobanidus/mods/lootr/common/mixins/MixinBrushItem.java
@@ -4,7 +4,9 @@ import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BrushItem;
 import net.minecraft.world.item.ItemStack;
@@ -21,8 +23,9 @@ public class MixinBrushItem {
   @Definition(id = "BrushableBlockEntity", type = BrushableBlockEntity.class)
   @Expression("? instanceof BrushableBlockEntity")
   @ModifyExpressionValue(method = "onUseTick", at = @At("MIXINEXTRAS:EXPRESSION"))
-  private boolean lootr$AllowBrushingOtherBlockEntities(boolean original, @Local(argsOnly = true) Level level, @Local BlockHitResult blockHitResult, @Local Player player, @Local(argsOnly = true) ItemStack itemStack, @Local BlockEntity blockEntity) {
-    if (blockEntity instanceof IBrushable brushable) {
+  private boolean lootr$AllowBrushingOtherBlockEntities(boolean original, @Local(argsOnly = true) Level level, @Local(argsOnly = true) LivingEntity livingEntity, @Local(argsOnly = true) ItemStack itemStack, @Local BlockHitResult blockHitResult) {
+    BlockPos pos = blockHitResult.getBlockPos();
+    if (level.getBlockEntity(pos) instanceof IBrushable brushable && livingEntity instanceof Player player) {
       boolean bl2 = brushable.IBrushable$brush(level.getGameTime(), player, blockHitResult.getDirection());
       if (bl2) {
         EquipmentSlot equipmentSlot = itemStack.equals(player.getItemBySlot(EquipmentSlot.OFFHAND)) ? EquipmentSlot.OFFHAND : EquipmentSlot.MAINHAND;
