26e02a8c9c191f0bf7c98d8d2437940775df4760
Jon <due@wxwhatever.com>
2025-12-08 15:14:41 +1000

    Jump through all the hoops to get mixinextras.

---
+++ common/src/main/java/noobanidus/mods/lootr/common/mixins/MixinBrushItem.java
@@ -0,0 +1,10 @@
+package noobanidus.mods.lootr.common.mixins;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.world.item.BrushItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.InjectionPoint;
+
+@Mixin(BrushItem.class)
+public class MixinBrushItem {
+}
