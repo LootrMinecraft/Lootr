2f0e8eda0daaa405791b1e33faf2ad65ea219e3d
Jon <due@wxwhatever.com>
2025-12-13 12:29:58 +1000

    Fix for #707: bonus chest not being converted.

--- common/src/main/java/noobanidus/mods/lootr/common/mixins/MixinRandomizableContainer.java
+++ common/src/main/java/noobanidus/mods/lootr/common/mixins/MixinRandomizableContainer.java
@@ -2,9 +2,11 @@ package noobanidus.mods.lootr.common.mixins;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.RandomizableContainer;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.storage.loot.LootTable;
 import noobanidus.mods.lootr.common.api.LootrAPI;
 import noobanidus.mods.lootr.common.api.data.blockentity.ILootrBlockEntity;
 import noobanidus.mods.lootr.common.block.entity.BlockEntityTicker;
@@ -16,6 +18,14 @@ public interface MixinRandomizableContainer {
   @WrapOperation(method = "tryLoadLootTable", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/RandomizableContainer;setLootTableSeed(J)V"))
   default void lootr$tryLoadLootTable(RandomizableContainer instance, long l, Operation<Void> original) {
     original.call(instance, l);
+    if (!(instance instanceof ILootrBlockEntity) && instance instanceof BlockEntity blockEntity && blockEntity.getLevel() != null && !(LootrAPI.resolveBlockEntity(blockEntity) instanceof ILootrBlockEntity)) {
+      BlockEntityTicker.addEntity(blockEntity, blockEntity.getLevel(), new ChunkPos(blockEntity.getBlockPos()));
+    }
+  }
+
+  @WrapOperation(method="setLootTable(Lnet/minecraft/resources/ResourceKey;J)V", at=@At(value="INVOKE", target="Lnet/minecraft/world/RandomizableContainer;setLootTable(Lnet/minecraft/resources/ResourceKey;)V"))
+  default void lootr$setLootTable(RandomizableContainer instance, ResourceKey<LootTable> table, Operation<Void> original) {
+    original.call(instance, table);
     if (!(instance instanceof ILootrBlockEntity) && instance instanceof BlockEntity blockEntity && blockEntity.getLevel() != null && !(LootrAPI.resolveBlockEntity(blockEntity) instanceof ILootrBlockEntity)) {
       BlockEntityTicker.addEntity(blockEntity, blockEntity.getLevel(), new ChunkPos(blockEntity.getBlockPos()));
     }
