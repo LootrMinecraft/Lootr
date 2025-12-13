ae64a86849b8b537294d68c1a7232cfb0bb615b5
Jon <due@wxwhatever.com>
2025-12-13 19:45:09 +1000

    Implement data access for pots.

--- fabric/src/main/java/noobanidus/mods/lootr/fabric/block/entity/LootrFabricDecoratedPotBlockEntity.java
+++ fabric/src/main/java/noobanidus/mods/lootr/fabric/block/entity/LootrFabricDecoratedPotBlockEntity.java
@@ -1,12 +1,25 @@
 package noobanidus.mods.lootr.fabric.block.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.state.BlockState;
 import noobanidus.mods.lootr.common.api.registry.LootrRegistry;
 import noobanidus.mods.lootr.common.block.entity.LootrDecoratedPotBlockEntity;
+import noobanidus.mods.lootr.common.client.ClientHooks;
+import org.jetbrains.annotations.Nullable;
 
 public class LootrFabricDecoratedPotBlockEntity extends LootrDecoratedPotBlockEntity {
   public LootrFabricDecoratedPotBlockEntity(BlockPos blockPos, BlockState blockState) {
     super(LootrRegistry.getDecoratedPotBlockEntity(), blockPos, blockState);
+  }
+
+  @Override
+  public @Nullable Object getRenderData() {
+    Player player = ClientHooks.getPlayer();
+    if (player == null) {
+      return null;
+    }
+
+    return hasClientOpened(player.getUUID());
   }
 }
