216998b3ab58e50b51b0c297c94de8699fd2b111
Jon <due@wxwhatever.com>
2025-12-22 14:57:19 +1000

    Adjust particle spawn location depending on openness.

--- common/src/main/java/noobanidus/mods/lootr/common/client/ClientHooks.java
+++ common/src/main/java/noobanidus/mods/lootr/common/client/ClientHooks.java
@@ -9,6 +9,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.state.BlockState;
+import noobanidus.mods.lootr.common.api.data.blockentity.ILootrBlockEntity;
 import noobanidus.mods.lootr.common.mixin.accessor.AccessorMixinBlock;
 import org.jetbrains.annotations.Nullable;
 
@@ -45,18 +46,24 @@ public class ClientHooks {
     if (!(mc.level.getEntity(entityId) instanceof Player player)) {
       return;
     }
+    double offset = 1.2;
     if (player == mc.player) {
       BlockState state = mc.level.getBlockState(pos);
       ((AccessorMixinBlock) state.getBlock()).lootr$spawnDestroyParticles(mc.level, player, pos, state);
       mc.level.playSound(null, pos, SoundEvents.DECORATED_POT_BREAK, SoundSource.BLOCKS, 1.0F, 1.0F);
     } else {
+      if (mc.level.getBlockEntity(pos) instanceof ILootrBlockEntity ibe) {
+        if (ibe.hasClientOpened(mc.player)) {
+          offset = 0.5;
+        }
+      }
       mc.level.playSound(null, pos, SoundEvents.DECORATED_POT_INSERT_FAIL, SoundSource.BLOCKS, 1.0F, 1.0F);
       for (int i = 0; i < 7; i++) {
         mc.level.addParticle(
             ParticleTypes.DUST_PLUME,
             false,
             pos.getX() + 0.5,
-            pos.getY() + 1.2,
+            pos.getY() + offset,
             pos.getZ() + 0.5,
             0,
             0,
