0acc216a79debdb57d44022b5387be82fb1f473c
Jon <due@wxwhatever.com>
2025-12-28 09:57:19 +1000

    Rejig chest block mixin.

--- common/src/main/java/noobanidus/mods/lootr/common/mixin/chest_blocking/MixinChestBlock.java
+++ common/src/main/java/noobanidus/mods/lootr/common/mixin/chest_blocking/MixinChestBlock.java
@@ -1,5 +1,8 @@
 package noobanidus.mods.lootr.common.mixin.chest_blocking;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.ChestBlock;
@@ -7,18 +10,17 @@ import net.minecraft.world.level.block.state.BlockState;
 import noobanidus.mods.lootr.common.api.LootrTags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChestBlock.class)
 public class MixinChestBlock {
-  @Inject(method = "isBlockedChestByBlock", at = @At("HEAD"), cancellable = true)
-  private static void LootrIsChestBlocked(BlockGetter blockGetter, BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
-    BlockState thisState = blockGetter.getBlockState(blockPos);
-    BlockState aboveState = blockGetter.getBlockState(blockPos.above());
-    if (thisState.is(LootrTags.Blocks.CONTAINERS) && aboveState.is(LootrTags.Blocks.NON_BLOCKING)) {
-      cir.setReturnValue(false);
-      cir.cancel();
+  @WrapOperation(method="isBlockedChestByBlock", at=@At(value="INVOKE", target="Lnet/minecraft/world/level/block/state/BlockState;isRedstoneConductor(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Z"))
+  private static boolean LootrIsChestBlocked(BlockState instance, BlockGetter blockGetter, BlockPos blockPos, Operation<Boolean> original, @Local(argsOnly = true) BlockPos pos) {
+    if (instance.is(LootrTags.Blocks.NON_BLOCKING)) {
+      BlockState blockState = blockGetter.getBlockState(pos);
+      if (blockState.is(LootrTags.Blocks.CONTAINERS)) {
+        return false;
+      }
     }
+    return original.call(instance, blockGetter, blockPos);
   }
 }
