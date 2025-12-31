dc53d05ea9381ece3f63da5cc22dded3318d8de7
Jon <due@wxwhatever.com>
2025-12-31 14:57:55 +1000

    Fix Lootrmon, etc, compatibility.

---
+++ common/src/main/java/noobanidus/mods/lootr/common/api/registry/LootrProperties.java
@@ -0,0 +1,16 @@
+package noobanidus.mods.lootr.common.api.registry;
+
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import noobanidus.mods.lootr.common.api.LootrConstants;
+
+@Deprecated
+public class LootrProperties extends LootrConstants {
+  public static final BlockBehaviour.Properties CHEST_PROPERTIES = LootrConstants.CHEST_PROPERTIES;
+  public static final BlockBehaviour.Properties BARREL_PROPERTIES = LootrConstants.BARREL_PROPERTIES;
+  public static final BlockBehaviour.Properties TRAPPED_CHEST_PROPERTIES = LootrConstants.TRAPPED_CHEST_PROPERTIES;
+  public static final BlockBehaviour.Properties SHULKER_BOX_PROPERTIES = LootrConstants.SHULKER_BOX_PROPERTIES;
+  public static final BlockBehaviour.Properties SUSPICIOUS_SAND_PROPERTIES = LootrConstants.SUSPICIOUS_SAND_PROPERTIES;
+  public static final BlockBehaviour.Properties SUSPICIOUS_GRAVEL_PROPERTIES = LootrConstants.SUSPICIOUS_GRAVEL_PROPERTIES;
+  public static final BlockBehaviour.Properties DECORATED_POT_PROPERTIES = LootrConstants.DECORATED_POT_PROPERTIES;
+
+}
