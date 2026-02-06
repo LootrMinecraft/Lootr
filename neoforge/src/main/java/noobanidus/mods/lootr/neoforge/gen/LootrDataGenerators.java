284e72fbc1cffaf350e8e4c5ecda7bbcaef23418
Jon <due@wxwhatever.com>
2026-02-06 13:53:08 +1000

    No, that was dumb.

--- neoforge/src/main/java/noobanidus/mods/lootr/neoforge/gen/LootrDataGenerators.java
+++ neoforge/src/main/java/noobanidus/mods/lootr/neoforge/gen/LootrDataGenerators.java
@@ -1,22 +1,14 @@
 package noobanidus.mods.lootr.neoforge.gen;
 
-import net.minecraft.DetectedVersion;
-import net.minecraft.SharedConstants;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.advancements.AdvancementProvider;
-import net.minecraft.data.metadata.PackMetadataGenerator;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import noobanidus.mods.lootr.common.api.LootrAPI;
 
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -39,16 +31,5 @@ public class LootrDataGenerators {
     generator.addProvider(true, new LootrLangProvider(output));
     generator.addProvider(true, new LootrParticleProvider(output));
     generator.addProvider(true, new LootrModelProvider(output));
-
-    Path root = output.getOutputFolder().getParent().getParent().getParent().getParent();
-    Path datapacks = root.resolve("neoforge").resolve("src").resolve("generated").resolve("resources"); // a hack
-
-    DataGenerator generator2 = new DataGenerator(datapacks, DetectedVersion.tryDetectVersion(), true);
-    generator2.addProvider(true,new LootrUnbakedModelProvider(output));
-    try {
-      generator2.run();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
   }
 }
