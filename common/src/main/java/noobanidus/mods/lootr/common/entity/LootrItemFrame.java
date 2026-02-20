3e3ecd16e2ffaf47d2b96321b01384973b9defa9
Jon <due@wxwhatever.com>
2026-02-20 13:21:51 +1000

    Reinstate item frame analogue output.
    This wasn't meant to be overridden.

--- common/src/main/java/noobanidus/mods/lootr/common/entity/LootrItemFrame.java
+++ common/src/main/java/noobanidus/mods/lootr/common/entity/LootrItemFrame.java
@@ -21,6 +21,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DiodeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
@@ -268,11 +269,6 @@ public class LootrItemFrame extends ItemFrame implements ILootrEntity {
     } else {
       return InteractionResult.SUCCESS;
     }
-  }
-
-  @Override
-  public int getAnalogOutput() {
-    return 0;
   }
 
   // TODO:
