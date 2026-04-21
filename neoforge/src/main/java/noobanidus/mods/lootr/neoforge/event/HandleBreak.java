c54a81f8963c7285e6174e083e655a7ed2f7a10d
Jon <due@wxwhatever.com>
2026-04-21 19:29:13 +1000

    Run the block break event on both sides.

--- neoforge/src/main/java/noobanidus/mods/lootr/neoforge/event/HandleBreak.java
+++ neoforge/src/main/java/noobanidus/mods/lootr/neoforge/event/HandleBreak.java
@@ -2,6 +2,8 @@ package noobanidus.mods.lootr.neoforge.event;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -14,16 +16,17 @@ import noobanidus.mods.lootr.common.api.data.blockentity.ILootrBlockEntity;
 @EventBusSubscriber(modid = LootrAPI.MODID)
 public class HandleBreak {
 
-  // TODO: Run on both sides
   @SubscribeEvent
   public static void onBlockBreak(BreakBlockEvent event) {
-    if (event.getLevel().isClientSide() || !(event.getState().is(LootrTags.Blocks.CONTAINERS))) {
+    if (!(event.getState().is(LootrTags.Blocks.CONTAINERS))) {
       return;
     }
 
+    LevelAccessor level = event.getLevel();
+
     Player player = event.getPlayer();
 
-    BlockEntity block = event.getLevel().getBlockEntity(event.getPos());
+    BlockEntity block = level.getBlockEntity(event.getPos());
     if (LootrAPI.wrapBlockEntity(block) instanceof ILootrBlockEntity lbe) {
       if (!lbe.hasLootTable() && !lbe.isDataReferenceInventory()) {
         return;
@@ -35,24 +38,30 @@ public class HandleBreak {
         if (player.getAbilities().instabuild) {
           if (!player.isShiftKeyDown()) {
             event.setCanceled(true);
-            player.sendSystemMessage(Component.translatable("lootr.message.cannot_break_sneak")
-                .setStyle(LootrAPI.getChatStyle()));
+            if (!level.isClientSide()) {
+              player.sendSystemMessage(Component.translatable("lootr.message.cannot_break_sneak")
+                  .setStyle(LootrAPI.getChatStyle()));
+            }
           }
         } else {
           event.setCanceled(true);
-          event.setNotifyClient(true);
-          player.sendSystemMessage(Component.translatable("lootr.message.cannot_break")
-              .setStyle(LootrAPI.getChatStyle()));
+          if (!level.isClientSide()) {
+            event.setNotifyClient(true);
+            player.sendSystemMessage(Component.translatable("lootr.message.cannot_break")
+                .setStyle(LootrAPI.getChatStyle()));
+          }
         }
       } else {
         if (!event.getPlayer().isShiftKeyDown()) {
           event.setCanceled(true);
-          event.setNotifyClient(true);
-          event.getPlayer().sendSystemMessage(Component.translatable("lootr.message.should_sneak")
-              .setStyle(LootrAPI.getChatStyle()));
-          event.getPlayer()
-              .sendSystemMessage(Component.translatable("lootr.message.should_sneak2")
-                  .setStyle(LootrAPI.getChatStyle()));
+          if (!level.isClientSide()) {
+            event.setNotifyClient(true);
+            event.getPlayer().sendSystemMessage(Component.translatable("lootr.message.should_sneak")
+                .setStyle(LootrAPI.getChatStyle()));
+            event.getPlayer()
+                .sendSystemMessage(Component.translatable("lootr.message.should_sneak2")
+                    .setStyle(LootrAPI.getChatStyle()));
+          }
         }
       }
     }
