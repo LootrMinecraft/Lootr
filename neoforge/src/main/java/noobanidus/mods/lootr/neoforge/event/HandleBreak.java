c4d0cb761e06c30e01d9231bf63b9d45d2030245
Jon <due@wxwhatever.com>
2026-04-21 12:11:07 +1000

    Update to new BreakBlockEvent.

--- neoforge/src/main/java/noobanidus/mods/lootr/neoforge/event/HandleBreak.java
+++ neoforge/src/main/java/noobanidus/mods/lootr/neoforge/event/HandleBreak.java
@@ -6,6 +6,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.neoforge.event.level.block.BreakBlockEvent;
 import noobanidus.mods.lootr.common.api.LootrAPI;
 import noobanidus.mods.lootr.common.api.LootrTags;
 import noobanidus.mods.lootr.common.api.data.blockentity.ILootrBlockEntity;
@@ -13,8 +14,9 @@ import noobanidus.mods.lootr.common.api.data.blockentity.ILootrBlockEntity;
 @EventBusSubscriber(modid = LootrAPI.MODID)
 public class HandleBreak {
 
+  // TODO: Run on both sides
   @SubscribeEvent
-  public static void onBlockBreak(BlockEvent.BreakEvent event) {
+  public static void onBlockBreak(BreakBlockEvent event) {
     if (event.getLevel().isClientSide() || !(event.getState().is(LootrTags.Blocks.CONTAINERS))) {
       return;
     }
@@ -38,12 +40,14 @@ public class HandleBreak {
           }
         } else {
           event.setCanceled(true);
+          event.setNotifyClient(true);
           player.sendSystemMessage(Component.translatable("lootr.message.cannot_break")
               .setStyle(LootrAPI.getChatStyle()));
         }
       } else {
         if (!event.getPlayer().isShiftKeyDown()) {
           event.setCanceled(true);
+          event.setNotifyClient(true);
           event.getPlayer().sendSystemMessage(Component.translatable("lootr.message.should_sneak")
               .setStyle(LootrAPI.getChatStyle()));
           event.getPlayer()
