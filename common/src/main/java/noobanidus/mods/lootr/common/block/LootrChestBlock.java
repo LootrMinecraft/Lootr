e3267877e04aff9b99f0e31456738b2f7cf9da0a
Jon <due@wxwhatever.com>
2026-07-03 11:35:59 +1000

    Fix #800: properly swing hand when opening chests.

--- common/src/main/java/noobanidus/mods/lootr/common/block/LootrChestBlock.java
+++ common/src/main/java/noobanidus/mods/lootr/common/block/LootrChestBlock.java
@@ -57,13 +57,12 @@ public class LootrChestBlock extends ChestBlock {
 
   @Override
   public @NonNull InteractionResult useWithoutItem(@NonNull BlockState state, Level level, @NonNull BlockPos pos, @NonNull Player player, @NonNull BlockHitResult trace) {
-    if (level.isClientSide() || player.isSpectator() || !(player instanceof ServerPlayer serverPlayer)) {
-      return InteractionResult.CONSUME;
-    }
-    if (serverPlayer.isShiftKeyDown()) {
-      LootrAPI.handleInstanceSneak(ILootrContainerInstance.of(pos, level), serverPlayer);
-    } else if (!isChestBlockedAt(level, pos)) {
-      LootrAPI.handleInstanceOpen(ILootrContainerInstance.of(pos, level), serverPlayer);
+    if (!level.isClientSide() && !player.isSpectator() && player instanceof ServerPlayer serverPlayer) {
+      if (serverPlayer.isShiftKeyDown()) {
+        LootrAPI.handleInstanceSneak(ILootrContainerInstance.of(pos, level), serverPlayer);
+      } else if (!isChestBlockedAt(level, pos)) {
+        LootrAPI.handleInstanceOpen(ILootrContainerInstance.of(pos, level), serverPlayer);
+      }
     }
     return InteractionResult.SUCCESS;
   }
