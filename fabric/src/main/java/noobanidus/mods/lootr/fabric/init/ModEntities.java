4d17e21fc8a1b0278531f21f0802cc143b3a72ab
Jon <due@wxwhatever.com>
2026-07-12 12:44:40 +1000

    Fix #806: alias lootr_minecart to lootr_cart.

--- fabric/src/main/java/noobanidus/mods/lootr/fabric/init/ModEntities.java
+++ fabric/src/main/java/noobanidus/mods/lootr/fabric/init/ModEntities.java
@@ -1,9 +1,11 @@
 package noobanidus.mods.lootr.fabric.init;
 
+import net.fabricmc.fabric.api.event.registry.FabricRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
+import noobanidus.mods.lootr.common.api.LootrAPI;
 import noobanidus.mods.lootr.common.api.LootrConstants;
 import noobanidus.mods.lootr.common.entity.LootrChestMinecartEntity;
 import noobanidus.mods.lootr.common.entity.LootrItemFrame;
@@ -13,11 +15,13 @@ public class ModEntities {
   public static EntityType<LootrItemFrame> ITEM_FRAME;
 
   public static void registerEntities() {
-    LOOTR_MINECART_ENTITY = Registry.register(BuiltInRegistries.ENTITY_TYPE, LootrConstants.LOOTR_CART, EntityType.Builder.of((EntityType.EntityFactory<LootrChestMinecartEntity>) LootrChestMinecartEntity::new, MobCategory.MISC).sized(0.9f, 1.4f).clientTrackingRange(8).build());
+    LOOTR_MINECART_ENTITY = Registry.register(BuiltInRegistries.ENTITY_TYPE, LootrConstants.LOOTR_CART, EntityType.Builder.of((EntityType.EntityFactory<LootrChestMinecartEntity>) LootrChestMinecartEntity::new, MobCategory.MISC)
+        .sized(0.9f, 1.4f).clientTrackingRange(8).build());
     ITEM_FRAME = Registry.register(BuiltInRegistries.ENTITY_TYPE, LootrConstants.ITEM_FRAME, EntityType.Builder.<LootrItemFrame>of(LootrItemFrame::new, MobCategory.MISC)
         .sized(0.5F, 0.5F)
         .eyeHeight(0.0F)
         .clientTrackingRange(10)
         .updateInterval(Integer.MAX_VALUE).build());
+    BuiltInRegistries.ENTITY_TYPE.addAlias(LootrAPI.rl("lootr_minecart"), LootrConstants.LOOTR_CART);
   }
 }
