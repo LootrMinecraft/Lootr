8252d0f324d132c21c05491c49589c98b631aae9
Jon <due@wxwhatever.com>
2026-07-12 13:16:38 +1000

    Fix #801: prevent hoppers into entities, blocks.

--- common/src/main/java/noobanidus/mods/lootr/common/mixin/hopper/MixinEntitySelector.java
+++ common/src/main/java/noobanidus/mods/lootr/common/mixin/hopper/MixinEntitySelector.java
@@ -11,9 +11,10 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(EntitySelector.class)
 public class MixinEntitySelector {
   // I'm not sure if this is really necessary
+  // TODO; Still not sure if this is necessary
   @WrapOperation(method = {"lambda$static$2", "method_5914"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;isAlive()Z"))
   private static boolean lootr$checkContainerEntitySelector(Entity instance, Operation<Boolean> original) {
-    if (instance.getType().is(LootrTags.Entity.CONVERT_ITEM_FRAMES)) {
+    if (instance.getType().is(LootrTags.Entity.CONTAINERS)) {
       return false;
     }
     return original.call(instance);
--- common/src/main/java/noobanidus/mods/lootr/common/mixin/hopper/MixinHopperBlockEntity.java
+++ common/src/main/java/noobanidus/mods/lootr/common/mixin/hopper/MixinHopperBlockEntity.java
@@ -1,22 +1,42 @@
 package noobanidus.mods.lootr.common.mixin.hopper;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.Container;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.HopperBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import noobanidus.mods.lootr.common.api.LootrAPI;
+import noobanidus.mods.lootr.common.api.LootrTags;
+import noobanidus.mods.lootr.common.api.data.blockentity.ILootrBlockEntity;
 import noobanidus.mods.lootr.common.api.data.entity.ILootrEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(HopperBlockEntity.class)
 public class MixinHopperBlockEntity {
-  // Prevents hoppers from hijacking container entities, however there's an issue
-  // because this doesn't fix EntitySelector.CONTAINER_ENTITY_SELECTOR
-  @WrapOperation(method = "getContainerAt(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;DDD)Lnet/minecraft/world/Container;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/HopperBlockEntity;getEntityContainer(Lnet/minecraft/world/level/Level;DDD)Lnet/minecraft/world/Container;"))
-  private static Container lootr$getEntityContainer(Level level, double x, double y, double z, Operation<Container> original) {
+  @WrapMethod(method = "getBlockContainer")
+  private static Container lootr$preventHopperBlockInteraction(Level level, BlockPos pos, BlockState state, Operation<Container> original) {
+    if (state.is(LootrTags.Blocks.CONTAINERS)) {
+      return null;
+    }
+
+    if (LootrAPI.resolveBlockEntity(level.getBlockEntity(pos)) instanceof ILootrBlockEntity) {
+      return null;
+    }
+
+    return original.call(level, pos, state);
+  }
+
+  @WrapMethod(method = "getEntityContainer")
+  private static Container lootr$preventHopperEntityInteraction(Level level, double x, double y, double z, Operation<Container> original) {
     var result = original.call(level, x, y, z);
-    if (result instanceof ILootrEntity) {
+    Entity entity = (Entity) result;
+    if (entity == null || entity.getType().is(LootrTags.Entity.CONTAINERS)) {
+      return null;
+    }
+    if (LootrAPI.resolveEntity(entity) instanceof ILootrEntity) {
       return null;
     }
 
