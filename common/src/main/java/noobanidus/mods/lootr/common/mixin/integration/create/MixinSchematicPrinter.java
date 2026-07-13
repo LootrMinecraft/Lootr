65d861274905bbba555f4fac8fd3cfb3d32830ac
Jon <due@wxwhatever.com>
2026-07-13 13:11:07 +1000

    Fix mixin for #848.
    Brings it into line with 1.21.1 but makes assumptions about how 26.1.2
    Create might be structured.

---
+++ common/src/main/java/noobanidus/mods/lootr/common/mixin/integration/create/MixinSchematicPrinter.java
@@ -0,0 +1,26 @@
+package noobanidus.mods.lootr.common.mixin.integration.create;
+
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import noobanidus.mods.lootr.common.integration.create.CreateIntegration;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Pseudo
+@Mixin(targets = {"com.simibubi.create.content.schematics.SchematicPrinter"})
+public class MixinSchematicPrinter {
+  @SuppressWarnings("UnresolvedMixinReference")
+  @Inject(method = "loadSchematic", at = @At("HEAD"))
+  private void lootr$preventItemFrameConversionHead(ItemStack blueprint, Level originalWorld, boolean processNBT, CallbackInfo cir) {
+    CreateIntegration.SKIP_ITEM_FRAMES = true;
+  }
+
+  @SuppressWarnings("UnresolvedMixinReference")
+  @Inject(method = "loadSchematic", at = @At("RETURN"))
+  private void lootr$preventItemFrameConversionTail(ItemStack blueprint, Level originalWorld, boolean processNBT, CallbackInfo cir) {
+    CreateIntegration.SKIP_ITEM_FRAMES = false;
+  }
+}
