4962028b3935598ea15b040a318d7940953a97de
Jon <due@wxwhatever.com>
2026-09-17 13:53:45 +1000

    Finish cherry-picking 751d5e2a

--- common/src/main/java/noobanidus/mods/lootr/common/client/ClientHooks.java
+++ common/src/main/java/noobanidus/mods/lootr/common/client/ClientHooks.java
@@ -1,6 +1,8 @@
 package noobanidus.mods.lootr.common.client;
 
+import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.SectionPos;
 import net.minecraft.core.particles.ParticleTypes;
@@ -8,13 +10,18 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import noobanidus.mods.lootr.common.api.PlayerContext;
+import noobanidus.mods.lootr.common.api.client.FrustumExtension;
 import noobanidus.mods.lootr.common.api.data.ILootrContainerInstance;
 import noobanidus.mods.lootr.common.api.data.blockentity.ILootrBlockEntity;
 import noobanidus.mods.lootr.common.api.LootrRegistry;
+import noobanidus.mods.lootr.common.api.data.entity.ILootrEntity;
 import noobanidus.mods.lootr.common.api.particle.ParticleColorOption;
 import noobanidus.mods.lootr.common.mixin.accessor.AccessorMixinBlock;
 import org.jetbrains.annotations.Nullable;
@@ -90,30 +97,61 @@ public class ClientHooks {
     return min + random.nextDouble() * (max - min);
   }
 
+  private static Frustum getFrustum () {
+    Minecraft mc= Minecraft.getInstance();
+    Camera camera =  mc.gameRenderer.getMainCamera();
+    Frustum frustum1 = camera.getCapturedFrustum();
+    if (frustum1 != null) {
+      return frustum1;
+    }
+    return camera.getCullFrustum();
+  }
+
+  public static boolean testFrustumContainsPoint (Vec3 position) {
+    Frustum frustum = getFrustum();
+    return ((FrustumExtension)frustum).lootr$isVisible(position);
+  }
+
+  private static boolean hasLineOfSightOfBlock (ILootrContainerInstance provider) {
+    Minecraft mc = Minecraft.getInstance();
+    if (mc.player == null || mc.level == null) {
+      return false;
+    }
+
+    Player player = mc.player;
+
+    Vec3 vec31 = provider.getDataVec();
+    if (!testFrustumContainsPoint(vec31.add(0, 0.5, 0))) {
+      return false;
+    }
+
+    Vec3 vec3 = player.getEyePosition();
+
+    return !(vec31.distanceTo(vec3) > 128);
+  }
+
   public static void performUnopenedParticles(ILootrContainerInstance provider) {
-    PlayerContext player = getPlayerContext();
-    if (player.hasPlayer()) {
+    PlayerContext context = getPlayerContext();
+    if (context.hasPlayer()) {
       Level level = Minecraft.getInstance().level;
-      if (level != null && provider.shouldDisplayParticles(player)) {
+      if (level != null && provider.shouldDisplayParticles(context)) {
         RandomSource random = Minecraft.getInstance().level.getRandom();
-        int color1 = provider.getParticleColor1(player);
-        int color2 = provider.getParticleColor2(player);
         if (random.nextInt(3) == 0) {
-          double xOff = bounded(random, provider.getParticleXBounds());
-          double zOff = bounded(random, provider.getParticleZBounds());
-          Vec3 pos = provider.getParticleCenter();
-          level.addParticle(
-              new ParticleColorOption(LootrRegistry.getUnopenedParticleType(),
-              color1,
-              color2,
-              false),
-              pos.x + xOff,
-              pos.y + provider.getParticleYOffset() + random.nextDouble() * 0.02,
-              pos.z + zOff,
-              0,
-              random.nextDouble() * 0.02,
-              0
-          );
+          if (hasLineOfSightOfBlock(provider)) {
+            double xOff = bounded(random, provider.getParticleXBounds());
+            double zOff = bounded(random, provider.getParticleZBounds());
+            Vec3 pos = provider.getParticleCenter();
+            int color = provider.getParticleColor(context);
+            level.addParticle(
+                new ParticleColorOption(LootrRegistry.getUnopenedParticleType(), color, color, false),
+                pos.x + xOff,
+                pos.y + provider.getParticleYOffset() + random.nextDouble() * 0.02,
+                pos.z + zOff,
+                0,
+                random.nextDouble() * 0.02,
+                0
+            );
+          }
         }
       }
     }
