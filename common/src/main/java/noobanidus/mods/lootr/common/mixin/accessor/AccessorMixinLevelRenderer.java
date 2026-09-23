55c186d159b0aeebfaef643d55616e5a298ee7af
Jon <due@wxwhatever.com>
2026-09-23 13:48:57 +1000

    Extend the section refresh to include block entities.

--- common/src/main/java/noobanidus/mods/lootr/common/mixin/accessor/AccessorMixinLevelRenderer.java
+++ common/src/main/java/noobanidus/mods/lootr/common/mixin/accessor/AccessorMixinLevelRenderer.java
@@ -1,15 +1,26 @@
 package noobanidus.mods.lootr.common.mixin.accessor;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.chunk.SectionRenderDispatcher;
 import net.minecraft.client.renderer.culling.Frustum;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Set;
 
 @Mixin(LevelRenderer.class)
 public interface AccessorMixinLevelRenderer {
   @Accessor("capturedFrustum")
-  Frustum lootr$getCapturedFrustum ();
+  Frustum lootr$getCapturedFrustum();
 
   @Accessor("cullingFrustum")
-  Frustum lootr$getCullingFrustum ();
+  Frustum lootr$getCullingFrustum();
+
+  @Accessor("visibleSections")
+  ObjectArrayList<SectionRenderDispatcher.RenderSection> lootr$getVisibleSections();
+
+  @Accessor("globalBlockEntities")
+  Set<BlockEntity> lootr$getGlobalBlockEntities();
 }
